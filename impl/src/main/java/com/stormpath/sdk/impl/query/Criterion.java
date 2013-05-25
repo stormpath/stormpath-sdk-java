ee888d896bbf8631f9e4fc000639469111b15356
Les Hazlewood <les@hazlewood.com>
2013-05-25 12:33:14 -0700

    Staging commit for current query api progress

---
+++ impl/src/main/java/com/stormpath/sdk/impl/query/Criterion.java
@@ -0,0 +1,9 @@
+package com.stormpath.sdk.impl.query;
+
+import java.io.Serializable;
+
+/**
+ * @since 2013-03-14
+ */
+public interface Criterion extends Serializable {
+}
---
+++ impl/src/main/java/com/stormpath/sdk/impl/query/QueryStringFactory.java
@@ -0,0 +1,7 @@
+package com.stormpath.sdk.impl.query;
+
+/**
+ * @since 2013-03-14
+ */
+public class QueryStringFactory {
+}
