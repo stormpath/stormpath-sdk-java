d3f26300d7ec9ac6c5f1d05d7a715e719c161374
Les Hazlewood <les@hazlewood.com>
2014-01-09 18:35:33 -0800

    Fixed #28: exception.getMessage() now reports error.getDeveloperMessage() instead of error.getMessage()

--- api/src/main/java/com/stormpath/sdk/resource/ResourceException.java
+++ api/src/main/java/com/stormpath/sdk/resource/ResourceException.java
@@ -16,6 +16,7 @@
 package com.stormpath.sdk.resource;
 
 import com.stormpath.sdk.error.Error;
+import com.stormpath.sdk.lang.Assert;
 
 /**
  * @since 0.2
@@ -24,28 +25,45 @@ public class ResourceException extends RuntimeException implements Error {
 
     private final Error error;
 
+    /**
+     * Ensures the message used for the exception (i.e. exception.getMessage()) reports the {@code developerMessage}
+     * returned by the Stormpath API Server.  The regular Stormpath response body {@code message} field is targeted
+     * at applicadtion end-users that could very likely be non-technical.  Since an exception should be helpful to
+     * developers, it is better to show a more technical message.
+     * <p/>
+     * Added as a fix for <a href="https://github.com/stormpath/stormpath-sdk-java/issues/28">Issue #28</a>
+     *
+     * @param error the response Error. Cannot be null.
+     * @return {@code error.getDeveloperMessage()}
+     * @since 0.9.2
+     */
+    private static String buildExceptionMessage(Error error) {
+        Assert.notNull(error, "Error argument cannot be null.");
+        return error.getDeveloperMessage();
+    }
+
     public ResourceException(Error error) {
-        super(error != null ? error.getMessage() : "");
+        super(buildExceptionMessage(error));
         this.error = error;
     }
 
     @Override
     public int getStatus() {
-        return error != null ? error.getStatus() : -1;
+        return error.getStatus();
     }
 
     @Override
     public int getCode() {
-        return error != null ? error.getCode() : -1;
+        return error.getCode();
     }
 
     @Override
     public String getDeveloperMessage() {
-        return error != null ? error.getDeveloperMessage() : null;
+        return error.getDeveloperMessage();
     }
 
     @Override
     public String getMoreInfo() {
-        return error != null ? error.getMoreInfo() : null;
+        return error.getMoreInfo();
     }
 }
