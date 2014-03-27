5d1bd7f24e3580a886adee88939639679dba81dc
Mario Antollini <mario@stormpath.com>
2014-03-27 10:40:48 -0300

    Modified Version.java to read version.properties file

--- impl/src/main/java/com/stormpath/sdk/impl/http/support/Version.java
+++ impl/src/main/java/com/stormpath/sdk/impl/http/support/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Stormpath, Inc.
+ * Copyright 2014 Stormpath, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,13 +15,13 @@
  */
 package com.stormpath.sdk.impl.http.support;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URL;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
- * @since 0.1
+ * @since 0.9.4
  */
 public class Version {
 
@@ -33,27 +33,15 @@ public class Version {
 
     private static String lookupClientVersion() {
         Class clazz = Version.class;
-        String className = clazz.getSimpleName() + ".class";
-        String classPath = clazz.getResource(className).toString();
-        if (!classPath.startsWith("jar")) {
-            // Class not from JAR
-            return "NOT-FROM-JAR";
+        String filePath = "/com/stormpath/sdk/version.properties";
+
+        try {
+            InputStream inputStream = clazz.getResourceAsStream(filePath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to obtain version from ["+ filePath + "].");
         }
-        String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF";
-        Manifest manifest = getManifest(manifestPath);
-        Attributes attr = manifest.getMainAttributes();
-        String value = attr.getValue("Implementation-Version");
-        if (value == null) {
-            throw new IllegalStateException("Unable to obtain 'Implementation-Version' property from manifest.");
-        }
-        return value;
     }
 
-    private static Manifest getManifest(String path) {
-        try {
-            return new Manifest(new URL(path).openStream());
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to obtain version from manifest path [" + path + "]");
-        }
-    }
 }
