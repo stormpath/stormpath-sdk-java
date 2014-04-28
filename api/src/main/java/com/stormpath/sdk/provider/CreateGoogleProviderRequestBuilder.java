95a511a8bc03cc4a649c5d21af50c2ac116d6141
Les Hazlewood <les@hazlewood.com>
2014-04-28 15:38:10 -0700

    Removed unused interfaces

--- api/src/main/java/com/stormpath/sdk/provider/CreateGoogleProviderRequestBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.stormpath.sdk.provider;
-
-public interface CreateGoogleProviderRequestBuilder<T extends CreateGoogleProviderRequestBuilder<T>> extends CreateProviderRequestBuilder<T> {
-
-    T setRedirectUri(String redirectUri);
-
-}
