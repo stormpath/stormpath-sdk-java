a95f5ecb2613d611339c7766cfa0f96b79b89d3c
Mario Antollini <mario@stormpath.com>
2014-05-07 13:29:17 -0300

    Method chaining added to PasswordResetToken

--- impl/src/main/java/com/stormpath/sdk/impl/account/DefaultPasswordResetToken.java
+++ impl/src/main/java/com/stormpath/sdk/impl/account/DefaultPasswordResetToken.java
@@ -54,8 +54,9 @@ public class DefaultPasswordResetToken extends AbstractResource implements Passw
     }
 
     @Override
-    public void setEmail(String email) {
+    public PasswordResetToken setEmail(String email) {
         setProperty(EMAIL, email);
+        return this;
     }
 
     @Override
