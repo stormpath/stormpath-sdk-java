60453504a24ed57910eeb9c2d31d2aef7cab5665
Les Hazlewood <les@hazlewood.com>
2014-05-29 14:25:15 -0700

    Updated JavaDoc per TenantActions interface change.

--- api/src/main/java/com/stormpath/sdk/tenant/Tenant.java
+++ api/src/main/java/com/stormpath/sdk/tenant/Tenant.java
@@ -19,15 +19,41 @@ import com.stormpath.sdk.resource.Resource;
 import com.stormpath.sdk.resource.Saveable;
 
 /**
+ * A {@code Tenant} represents a customer's private data 'space' within Stormpath that contains all of the customer's
+ * Stormpath-stored resources, like {@link com.stormpath.sdk.application.Application Applications},
+ * {@link com.stormpath.sdk.directory.Directory Directories}, {@link com.stormpath.sdk.account.Account Accounts} and
+ * {@link com.stormpath.sdk.group.Group Groups}.
+ * <h3>TenantActions</h3>
+ * <p>
+ * Additionally, while this {@code Tenant} interface represents the actual Tenant resource, many tenant
+ * behaviors/actions are defined in the parent {@link TenantActions} interface.  {@link TenantActions} is
+ * implemented by both the Tenant implementation <em>and</em> the {@link com.stormpath.sdk.client.Client Client}.
+ * This is a convenience where common tenant-wide actions may be performed directly via the Client instance.  For
+ * example:
+ * <pre>
+ * ApplicationList myApplications = client.getApplications();
+ * </pre>
+ * instead of forcing you to make an intermediate {@code getCurrentTenant()} call:
+ * <pre>
+ * ApplicationList myApplications = client.getCurrentTenant().getApplications();
+ * </pre>
+ * which can sometimes make for more readable code.
+ * </p>
+ * <p>If you can't find what you're looking for in this interface, be sure to check {@link TenantActions} and it will
+ * likely be there.</p>
+ *
+ * @see TenantActions
+ *
  * @since 0.1
  */
 public interface Tenant extends Resource, Saveable, TenantActions {
 
     /**
-     * Returns the tenant's globally-unique name in Stormpath.
-     * <p/>
+     * <p>Returns the tenant's globally-unique name in Stormpath.</p>
+     * <p>
      * <b>THIS CAN CHANGE IN THE FUTURE.  Do not rely on it as a permanent identifier.</b>  If you need a permanent ID,
      * use the {@link #getHref() href} as the permanent ID (this is true for all resources, not just Tenant resources).
+     * </p>
      *
      * @return the tenant's Stormpath globally-unique name. THIS CAN CHANGE. Do not rely on it as a permanent
      *         identifier.
@@ -35,10 +61,11 @@ public interface Tenant extends Resource, Saveable, TenantActions {
     String getName();
 
     /**
-     * Returns the tenant's globally-unique human-readable key in Stormpath.
-     * <p/>
-     * <b>THIS CAN CHANGE IN THE FUTURE.  Do not rely on it as a permanent identifier.</b>  If you need a permanent ID,
+     * <p>Returns the tenant's globally-unique human-readable key in Stormpath.</p>
+     *
+     * <p><b>THIS CAN CHANGE IN THE FUTURE.  Do not rely on it as a permanent identifier.</b>  If you need a permanent ID,
      * use the {@link #getHref() href} as the permanent ID (this is true for all resources, not just Tenant resources).
+     * </p>
      *
      * @return the tenant's Stormpath globally-unique human-readable name key. THIS CAN CHANGE. Do not rely on it as a
      *         permanent identifier.
