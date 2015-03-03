d198ac0fc3368a003b881c5d53f42cfe6c7cde5d
Mario <mario@stormpath.com>
2015-03-03 14:13:48 -0300

    Improved Javadoc and added ITs

--- api/src/main/java/com/stormpath/sdk/account/Account.java
+++ api/src/main/java/com/stormpath/sdk/account/Account.java
@@ -450,7 +450,8 @@ public interface Account extends Resource, Saveable, Deletable, Extendable {
     ApiKey createApiKey(ApiKeyOptions options);
 
     /**
-     * Returns a paginated list of all of the account's {@link com.stormpath.sdk.application.Application Application} resources.
+     * Returns a paginated list of the applications that the account may login to. You may control which accounts may log in
+     * to specific applications by managing an application's mapped {@link com.stormpath.sdk.directory.AccountStore AccountStore}s.
      * <p/>
      * Tip: Instead of iterating over all applications, it might be more convenient (and practical) to execute a search
      * for one or more applications using the {@link #getApplications(ApplicationCriteria)} method instead of this one.
@@ -458,28 +459,34 @@ public interface Account extends Resource, Saveable, Deletable, Extendable {
      * @return a paginated list of all of the Account's {@link com.stormpath.sdk.application.Application Application} resources.
      * @see #getApplications(com.stormpath.sdk.application.ApplicationCriteria)
      * @see #getApplications(java.util.Map)
+     * @see com.stormpath.sdk.application.Application#getAccountStoreMappings()
+     * @see com.stormpath.sdk.application.Application#addAccountStore(com.stormpath.sdk.directory.AccountStore)
      * @since 1.0.RC4
      */
     ApplicationList getApplications();
 
     /**
-     * Returns a paginated list of the account's applications that match the specified query criteria.
+     * Returns a paginated list of the applications that the account may login to that match the specified query criteria.
+     * You may control which accounts may log in to specific applications by managing an application's mapped
+     * {@link com.stormpath.sdk.directory.AccountStore AccountStore}s.
      * <p/>
      * Each {@code queryParams} key/value pair will be converted to String name to String value pairs and appended to
      * the resource URL as query parameters, for example:
      * <pre>
-     * .../accounts/accountId/applications?param1=value1&param2=value2&...
+     * .../accounts/accountId/applications?param1=value1&amp;param2=value2&amp;...
      * </pre>
      *
      * @param queryParams the query parameters to use when performing a request to the collection.
      * @return a paginated list of the Account's applications that match the specified query criteria.
+     * @see com.stormpath.sdk.application.Application#getAccountStoreMappings()
+     * @see com.stormpath.sdk.application.Application#addAccountStore(com.stormpath.sdk.directory.AccountStore)
      * @since 1.0.RC4
      */
     ApplicationList getApplications(Map<String, Object> queryParams);
 
     /**
-     * Returns a paginated list of the account's applications that match the specified query criteria.  The
-     * {@link com.stormpath.sdk.application.Applications Applications} utility class is available to help construct
+     * Returns a paginated list of the applications that the account may login to that match the specified query criteria.
+     * The {@link com.stormpath.sdk.application.Applications Applications} utility class is available to help construct
      * the criteria DSL. For example:
      * <pre>
      * account.getApplications(Applications
@@ -493,6 +500,8 @@ public interface Account extends Resource, Saveable, Deletable, Extendable {
      *
      * @param criteria the  the query parameters to use when performing a request to the collection.
      * @return a paginated list of the Account's applications that match the specified query criteria.
+     * @see com.stormpath.sdk.application.Application#getAccountStoreMappings()
+     * @see com.stormpath.sdk.application.Application#addAccountStore(com.stormpath.sdk.directory.AccountStore)
      * @since 1.0.RC4
      */
     ApplicationList getApplications(ApplicationCriteria criteria);
