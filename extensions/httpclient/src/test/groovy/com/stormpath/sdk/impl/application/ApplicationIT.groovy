9dc83c62db9a167c9a1892ee18bb822c0584b7d8
Mario <mario@stormpath.com>
2015-03-30 23:12:30 -0300

    Added fix for failling ITs from sporadic_IT_errors_fix

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/application/ApplicationIT.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/application/ApplicationIT.groovy
@@ -599,49 +599,66 @@ class ApplicationIT extends ClientIT {
      */
     @Test
     void testAddAccountStore_Dirs() {
-        Directory dir = client.instantiate(Directory)
-        dir.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Dirs")
-        dir.description = dir.name + "-Description"
-        dir = client.currentTenant.createDirectory(dir);
-        deleteOnTeardown(dir)
+        def count = 1
+        while (count >= 0) { //re-trying once
+            try {
+                Directory dir = client.instantiate(Directory)
+                dir.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Dirs")
+                dir.description = dir.name + "-Description"
+                dir = client.currentTenant.createDirectory(dir);
+                deleteOnTeardown(dir)
 
-        def app = createTempApp()
+                def app = createTempApp()
 
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        def retrievedAccountStoreMapping = app.addAccountStore(dir.name)
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                def retrievedAccountStoreMapping = app.addAccountStore(dir.name)
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        retrievedAccountStoreMapping = app.addAccountStore(dir.href)
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                retrievedAccountStoreMapping = app.addAccountStore(dir.href)
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        retrievedAccountStoreMapping = app.addAccountStore(Directories.criteria().add(Directories.description().eqIgnoreCase(dir.description)))
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                retrievedAccountStoreMapping = app.addAccountStore(Directories.criteria().add(Directories.description().eqIgnoreCase(dir.description)))
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, dir.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        //Non-existent
-        retrievedAccountStoreMapping = app.addAccountStore("non-existent Dir")
-        assertNull(retrievedAccountStoreMapping)
+                //Non-existent
+                retrievedAccountStoreMapping = app.addAccountStore("non-existent Dir")
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(dir.name.substring(0, 10))
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(dir.name.substring(0, 10))
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(dir.name + "XXX")
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(dir.name + "XXX")
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(dir.href + "XXX")
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(dir.href + "XXX")
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(Directories.criteria().add(Directories.description().eqIgnoreCase(dir.description + "XXX")))
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(Directories.criteria().add(Directories.description().eqIgnoreCase(dir.description + "XXX")))
+                assertNull(retrievedAccountStoreMapping)
+            } catch (Exception e) {
+                if (!(e instanceof ResourceException)) {
+                    //this is the known sporadic exception; we will fail if there is a different one
+                    throw e;
+                }
+                count--
+                if (count < 0) {
+                    throw e
+                }
+                System.out.println("Test failed due to " + e.getMessage() + ".\nRetrying " + (count + 1) + " more time(s)")
+                continue
+            }
+            break;  //no error, let's get out of the loop
+        }
     }
 
     /**
@@ -649,55 +666,71 @@ class ApplicationIT extends ClientIT {
      */
     @Test
     void testAddAccountStore_Groups() {
+        def count = 1
+        while (count >= 0) { //re-trying once
+            try {
+                Directory dir = client.instantiate(Directory)
+                dir.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Groups")
+                dir.description = dir.name + "-Description"
+                dir = client.currentTenant.createDirectory(dir);
+                deleteOnTeardown(dir)
 
-        Directory dir = client.instantiate(Directory)
-        dir.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Groups")
-        dir.description = dir.name + "-Description"
-        dir = client.currentTenant.createDirectory(dir);
-        deleteOnTeardown(dir)
+                Group group = client.instantiate(Group)
+                group.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Groups")
+                group.description = group.name + "-Description"
+                dir.createGroup(group)
 
-        Group group = client.instantiate(Group)
-        group.name = uniquify("Java SDK: ApplicationIT.testAddAccountStore_Groups")
-        group.description = group.name + "-Description"
-        dir.createGroup(group)
+                def app = createTempApp()
 
-        def app = createTempApp()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                def retrievedAccountStoreMapping = app.addAccountStore(group.name)
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        def retrievedAccountStoreMapping = app.addAccountStore(group.name)
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                retrievedAccountStoreMapping = app.addAccountStore(group.href)
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        retrievedAccountStoreMapping = app.addAccountStore(group.href)
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                retrievedAccountStoreMapping = app.addAccountStore(Groups.criteria().add(Groups.description().eqIgnoreCase(group.description)))
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
+                assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
+                retrievedAccountStoreMapping.delete()
+                assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
 
-        retrievedAccountStoreMapping = app.addAccountStore(Groups.criteria().add(Groups.description().eqIgnoreCase(group.description)))
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 2)
-        assertEquals(retrievedAccountStoreMapping.accountStore.href, group.href)
-        retrievedAccountStoreMapping.delete()
-        assertAccountStoreMappingListSize(app.getAccountStoreMappings(), 1)
+                //Non-existent
+                retrievedAccountStoreMapping = app.addAccountStore("non-existent Group")
+                assertNull(retrievedAccountStoreMapping)
 
-        //Non-existent
-        retrievedAccountStoreMapping = app.addAccountStore("non-existent Group")
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(group.name.substring(0, 10))
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(group.name.substring(0, 10))
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(group.href + "XXX")
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(group.href + "XXX")
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(group.name + "XXX")
+                assertNull(retrievedAccountStoreMapping)
 
-        retrievedAccountStoreMapping = app.addAccountStore(group.name + "XXX")
-        assertNull(retrievedAccountStoreMapping)
-
-        retrievedAccountStoreMapping = app.addAccountStore(Groups.criteria().add(Groups.description().eqIgnoreCase(group.description + "XXX")))
-        assertNull(retrievedAccountStoreMapping)
+                retrievedAccountStoreMapping = app.addAccountStore(Groups.criteria().add(Groups.description().eqIgnoreCase(group.description + "XXX")))
+                assertNull(retrievedAccountStoreMapping)
+            } catch (Exception e) {
+                if (!(e instanceof ResourceException)) {
+                    //this is the known sporadic exception; we will fail if there is a different one
+                    throw e;
+                }
+                count--
+                if (count < 0) {
+                    throw e
+                }
+                System.out.println("Test failed due to " + e.getMessage() + ".\nRetrying " + (count + 1) + " more time(s)")
+                continue
+            }
+            break;  //no error, let's get out of the loop
+        }
     }
 
 
