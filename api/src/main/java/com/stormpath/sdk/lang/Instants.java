5ba6dc020d4410e0d0711879e7241c207f1cdd20
Anai Arroyo <anai.arroyo@stormpath.com>
2015-04-01 22:30:34 -0600

    Changes to code to expose and query by creation and modification timestamps (JSDK-181)

--- api/src/main/java/com/stormpath/sdk/lang/Instants.java
+++ api/src/main/java/com/stormpath/sdk/lang/Instants.java
@@ -15,9 +15,158 @@
 */
 package com.stormpath.sdk.lang;
 
+import java.time.*;
+import java.util.*;
+import java.util.Date;
+
 /**
  * @since 1.0.RC4
  */
 public class Instants {
+
+    public static final TimeZone UTC_TIMEZONE = TimeZone.getTimeZone("UTC");
+    public static final ZoneId UTC_ZONE_ID = UTC_TIMEZONE.toZoneId();
+
+    /**
+     * Converts a given time from UTC to the corresponding time in the specified {@link TimeZone}
+     * @param time the UTC instant to convert, represented as the number of milliseconds that have elapsed since midnight, January 1, 1970
+     * @param to the target {@link TimeZone} for the conversion
+     * @return the long representation of the instant converted to the specified {@link TimeZone}
+     */
+    public static long convertDateToLocalTime(long time, TimeZone to) {
+        return convertDate(time, UTC_TIMEZONE, to);
+    }
+
+    /**
+     * Converts a given time from the specified {@link TimeZone} to the corresponding UTC time
+     * @param time the instant to convert, represented as the number of milliseconds that have elapsed since midnight, January 1, 1970
+     * @param from the original {@link TimeZone}
+     * @return the UTC instant, represented as the number of milliseconds that have elapsed since midnight, January 1, 1970
+     */
+    public static long convertDateToUTC(long time, TimeZone from) {
+        return convertDate(time, from, UTC_TIMEZONE);
+    }
+
+    /**
+     * Converts a given time from a {@link TimeZone} to another
+     * @param time the instant to convert, represented as the number of milliseconds that have elapsed since midnight, January 1, 1970 in the {@code from} {@link TimeZone}
+     * @param from the original {@link TimeZone}
+     * @param to the target {@link TimeZone} for the conversion
+     * @return the long representation of the instant converted to the specified {@code to} {@link TimeZone}
+     */
+    public static long convertDate(long time, TimeZone from, TimeZone to) {
+        return time + getTimeZoneOffset(time, from, to);
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year){
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.JANUARY, 1, 0, 0);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year} and {@code month}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @param month  the month-of-year to represent, from 1 (January) to 12 (December)
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year, int month){
+        Assert.isTrue(1 <= month && month <= 12, "month param must be a value from 1 (January) to 12 (December)");
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.of(month), 1, 0, 0);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year}, {@code month} and {@code day}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @param month  the month-of-year to represent, from 1 (January) to 12 (December)
+     * @param day  the day-of-month to represent, from 1 to 31
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year, int month, int day){
+        Assert.isTrue(1 <= month && month <= 12, "month param must be a value from 1 (January) to 12 (December)");
+        Assert.isTrue(1 <= day && day <= 31, "day param must be a value from 1 to 31");
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.of(month), day, 0, 0);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year}, {@code month}, {@code day} and {@code hour}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @param month  the month-of-year to represent, from 1 (January) to 12 (December)
+     * @param day  the day-of-month to represent, from 1 to 31
+     * @param hour  the hour-of-day to represent, from 0 to 23
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year, int month, int day, int hour){
+        Assert.isTrue(1 <= month && month <= 12, "month param must be a value from 1 (January) to 12 (December)");
+        Assert.isTrue(1 <= day && day <= 31, "day param must be a value from 1 to 31");
+        Assert.isTrue(0 <= hour && hour <= 23, "hour param must be a value from 0 to 23");
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.of(month), day, hour, 0);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year}, {@code month}, {@code day}, {@code hour} and {@code minute}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @param month  the month-of-year to represent, from 1 (January) to 12 (December)
+     * @param day  the day-of-month to represent, from 1 to 31
+     * @param hour  the hour-of-day to represent, from 0 to 23
+     * @param minute  the minute-of-hour to represent, from 0 to 59
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year, int month, int day, int hour, int minute){
+        Assert.isTrue(1 <= month && month <= 12, "month param must be a value from 1 (January) to 12 (December)");
+        Assert.isTrue(1 <= day && day <= 31, "day param must be a value from 1 to 31");
+        Assert.isTrue(0 <= hour && hour <= 23, "hour param must be a value from 0 to 23");
+        Assert.isTrue(0 <= minute && minute <= 59, "minute param must be a value from 0 to 59");
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.of(month), day, hour, minute);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    /**
+     * Creates an UTC-based {@Link Date} from the provided {@code year}, {@code month}, {@code day}, {@code hour}, {@code minute} and {@code second}
+     * @param year  the year to represent, from MIN_YEAR to MAX_YEAR
+     * @param month  the month-of-year to represent, from 1 (January) to 12 (December)
+     * @param day  the day-of-month to represent, from 1 to 31
+     * @param hour  the hour-of-day to represent, from 0 to 23
+     * @param minute  the minute-of-hour to represent, from 0 to 59
+     * @param second  the second-of-hour to represent, from 0 to 59
+     * @return the UTC-based {@link Date}
+     */
+    public static Date of(int year, int month, int day, int hour, int minute, int second){
+        Assert.isTrue(1 <= month && month <= 12, "month param must be a value from 1 (January) to 12 (December)");
+        Assert.isTrue(1 <= day && day <= 31, "day param must be a value from 1 to 31");
+        Assert.isTrue(0 <= hour && hour <= 23, "hour param must be a value from 1 to 23");
+        Assert.isTrue(0 <= minute && minute <= 59, "minute param must be a value from 0 to 59");
+        Assert.isTrue(0 <= second && second <= 59, "second param must be a value from 0 to 59");
+        LocalDateTime zonedDateTime = LocalDateTime.of(year, Month.of(month), day, hour, minute, second);
+        return Date.from(zonedDateTime.atZone(UTC_ZONE_ID).toInstant());
+    }
+
+    private static long getTimeZoneOffset(long time, TimeZone from, TimeZone to) {
+        int fromOffset = from.getOffset(time);
+        int toOffset = to.getOffset(time);
+        int diff = 0;
+
+        if (fromOffset >= 0){
+            if (toOffset > 0){
+                toOffset = -1*toOffset;
+            } else {
+                toOffset = Math.abs(toOffset);
+            }
+            diff = (fromOffset+toOffset)*-1;
+        } else {
+            if (toOffset <= 0){
+                toOffset = -1*Math.abs(toOffset);
+            }
+            diff = (Math.abs(fromOffset)+toOffset);
+        }
+        return diff;
+    }
 
 }
---
+++ impl/src/test/groovy/com/stormpath/sdk/impl/lang/InstantsTest.groovy
@@ -0,0 +1,119 @@
+/*
+* Copyright 2015 Stormpath, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.stormpath.sdk.impl.lang
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat
+import com.stormpath.sdk.lang.Instants
+import org.testng.annotations.Test
+import java.text.DateFormat
+import static org.testng.Assert.assertEquals
+import static org.testng.Assert.fail
+
+/**
+ * @since 1.0.RC4
+ */
+class InstantsTest {
+
+    private static final DateFormat dateFormat = new ISO8601DateFormat();
+
+    @Test
+    void testMethodErrors(){
+        try {
+            Instants.of(1920, 15);
+            fail("Should have thrown")
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "month param must be a value from 1 (January) to 12 (December)")
+        }
+
+        try {
+            Instants.of(1920, 3, 0);
+            fail("Should have thrown")
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "day param must be a value from 1 to 31")
+        }
+
+        try {
+            Instants.of(1920, 3, 3, 24);
+            fail("Should have thrown")
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "hour param must be a value from 0 to 23")
+        }
+
+        try {
+            Instants.of(1920, 3, 3, 20, 60);
+            fail("Should have thrown")
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "minute param must be a value from 0 to 59")
+        }
+
+        try {
+            Instants.of(1920, 3, 3, 20, 28, 60);
+            fail("Should have thrown")
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "second param must be a value from 0 to 59")
+        }
+    }
+
+    @Test
+    void testMethods() {
+        TimeZone tz = TimeZone.getTimeZone("Africa/Algiers")
+        TimeZone utc = TimeZone.getTimeZone("UTC")
+
+        Calendar date2 = new GregorianCalendar(2014,2,15,10,0,0)
+        String expectedDate = "2014-03-15T08:00:00Z"
+        date2.setTimeZone(tz)   //for tz the set time is = 2014-03-15T09:00:00Z
+        long utcTimeStamp = Instants.convertDateToUTC(date2.getTimeInMillis(), tz)
+        Calendar utcCal = new GregorianCalendar(utc)
+        utcCal.setTimeInMillis(utcTimeStamp);
+        assertEquals(expectedDate, dateFormat.format(utcCal.getTime()))
+
+        expectedDate = "2014-03-15T09:00:00Z"
+        long lTime = Instants.convertDateToLocalTime(utcTimeStamp, tz);
+        Calendar locCal = new GregorianCalendar(tz)
+        locCal.setTimeInMillis(lTime);
+        assertEquals(expectedDate, dateFormat.format(locCal.getTime()))
+
+        // of(year)
+        expectedDate = "2004-01-01T00:00:00Z"
+        Date expected = Instants.of(2004);
+        assertEquals(dateFormat.format(expected), expectedDate);
+
+        // of(year, month)
+        expectedDate = "2011-11-01T00:00:00Z"
+        expected = Instants.of(2011, 11);
+        assertEquals(dateFormat.format(expected), expectedDate);
+
+        // of(year, month, day)
+        expectedDate = "2009-04-10T00:00:00Z"
+        expected = Instants.of(2009, 4, 10);
+        assertEquals(dateFormat.format(expected), expectedDate);
+
+        // of(year, month, day, hour)
+        expectedDate = "2015-09-10T20:00:00Z"
+        expected = Instants.of(2015, 9, 10, 20);
+        assertEquals(dateFormat.format(expected), expectedDate);
+
+        // of(year, month, day, hour, minute)
+        expectedDate = "2009-04-10T20:45:00Z"
+        expected = Instants.of(2009, 4, 10, 20, 45);
+        assertEquals(dateFormat.format(expected), expectedDate);
+
+        // of(year, month, day, hour, minute, second)
+        expectedDate = "2009-04-10T20:45:23Z"
+        expected = Instants.of(2009, 4, 10, 20, 45, 23);
+        assertEquals(dateFormat.format(expected), expectedDate);
+    }
+}
