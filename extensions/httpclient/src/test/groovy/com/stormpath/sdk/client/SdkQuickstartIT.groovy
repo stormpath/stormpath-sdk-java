67cdb98cf4a7f4625d3eefd98dc296d91c1ff7a4
Anai Arroyo <anai.arroyo@stormpath.com>
2015-04-17 14:08:27 -0600

    Onboarding Java Quickstart Guide issues. JSDK-189

---
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/client/SdkQuickstartIT.groovy
@@ -0,0 +1,31 @@
+/*
+* Copyright 2015 Stormpath, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.stormpath.sdk.client
+
+import org.testng.annotations.Test
+
+/**
+ * Password Reset Integration Tests for Stormpath developers.
+ *
+ * To function, this test must be enabled (enabled=true) and the {@code email} and {@code spToken] values must be set
+ * to values reflected in the developer's local development datastore.
+ *
+ * @since 1.0-SNAPSHOT
+ */
+@Test
+class SdkQuickstartIT {
+
+}
