3b14dee51056efd1c37bf9da1a66b1e3a44f2079
Mario <mario@stormpath.com>
2015-07-15 08:55:29 -0300

    Forcing travis build - 5

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/tenant/TenantIT.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/tenant/TenantIT.groovy
@@ -481,7 +481,6 @@ class TenantIT extends ClientIT {
 
         assertTrue tenantProperties.get("groups").get("size") == groupsQty + 2
         assertTrue tenantProperties.get("applications").get("size") == applicationsQty + 1
-
     }
 
     /**
