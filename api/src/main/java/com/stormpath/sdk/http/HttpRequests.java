808abad94765f4b8bddcbb779e25973669ce21de
Anai Arroyo <anai.arroyo@stormpath.com>
2015-07-17 20:07:11 -0600

    #225 Request Builder - simplify the HTTP builder so you dont have to deal with maps

--- api/src/main/java/com/stormpath/sdk/http/HttpRequests.java
+++ api/src/main/java/com/stormpath/sdk/http/HttpRequests.java
@@ -45,7 +45,6 @@ public final class HttpRequests {
     private static final Class<HttpRequestBuilder> HTTP_REQUEST_BUILDER =
         Classes.forName("com.stormpath.sdk.impl.http.DefaultHttpRequestBuilder");
 
-
     /**
      * Creates and returns a new {@link HttpRequestBuilder} that builds request instances that will be used for request
      * authentication via {@link com.stormpath.sdk.application.Application#authenticateApiRequest(Object)
