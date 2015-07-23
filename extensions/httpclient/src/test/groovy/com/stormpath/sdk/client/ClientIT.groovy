2481d7b03224d604cf553b93cbfa92b54924692c
Anai Arroyo <anai.arroyo@stormpath.com>
2015-07-22 22:52:35 -0600

    Password Import MCF Feature

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/client/ClientIT.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/client/ClientIT.groovy
@@ -35,7 +35,7 @@ abstract class ClientIT {
 
     private static final Logger log = LoggerFactory.getLogger(ClientIT)
 
-    String baseUrl = 'http://localhost:9191/v1'
+    String baseUrl = 'https://api.stormpath.com/v1'
     Client client
 
     List<Deletable> resourcesToDelete;
@@ -74,8 +74,6 @@ abstract class ClientIT {
     Client buildClient(boolean enableCaching=true) {
 
         def builder = Clients.builder()
-        String path = System.getProperty("user.home") + "/.stormpath/apiKey.properties";
-        builder.setApiKey(ApiKeys.builder().setFileLocation(path).build()).build();
         ((DefaultClientBuilder)builder).setBaseUrl(baseUrl)
 
         if (!enableCaching) {
@@ -88,8 +86,6 @@ abstract class ClientIT {
     Client buildClient(AuthenticationScheme authenticationScheme) {
 
         def builder = Clients.builder()
-        String path = System.getProperty("user.home") + "/.stormpath/apiKey.properties";
-        builder.setApiKey(ApiKeys.builder().setFileLocation(path).build()).build();
         ((DefaultClientBuilder)builder).setBaseUrl(baseUrl)
 
         builder.setAuthenticationScheme(authenticationScheme)
