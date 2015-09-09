eb28bd44c3a7b7521a4d4b6ba61ad9f4a424ca0f
Anai Arroyo <anai.arroyo@stormpath.com>
2015-09-08 19:51:11 -0600

    #260 Organization Resource

---
+++ api/src/main/java/com/stormpath/sdk/organization/Organizations.java
@@ -0,0 +1,247 @@
+/*
+* Copyright 2015 Stormpath, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.stormpath.sdk.organization;
+
+import com.stormpath.sdk.lang.Classes;
+import com.stormpath.sdk.query.Criterion;
+import com.stormpath.sdk.query.DateExpressionFactory;
+import com.stormpath.sdk.query.EqualsExpressionFactory;
+import com.stormpath.sdk.query.StringExpressionFactory;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Static utility/helper methods for working with {@link Organization} resources.  Most methods are
+ * <a href="http://en.wikipedia.org/wiki/Factory_method_pattern">factory method</a>s used for forming
+ * Organization-specific <a href="http://en.wikipedia.org/wiki/Fluent_interface">fluent DSL</a> queries. For example:
+ * <pre>
+ * <b>Organizations.where(Organizations.name()</b>.containsIgnoreCase("Foo")<b>)</b>
+ *     .and(<b>Organizations.status()</b>.eq(OrganizationStatus.ENABLED))
+ *     .orderByName().descending()
+ *     .offsetBy(50)
+ *     .limitTo(25));
+ * </pre>
+ * or, if using static imports:
+ * <pre>
+ * import static com.stormpath.sdk.organization.Organizations.*;
+ *
+ * ...
+ *
+ * <b>where(name()</b>.containsIgnoreCase("Foo")<b>)</b>
+ *     .and(<b>status()</b>.eq(OrganizationStatus.ENABLED))
+ *     .orderByName().descending()
+ *     .offsetBy(50)
+ *     .limitTo(25));
+ * </pre>
+ *
+ * @since 1.0.RC4.6
+ */
+public final class Organizations {
+
+    /**
+     * Returns a new {@link OrganizationOptions} instance, used to customize how one or more {@link Organization}(s) are retrieved.
+     *
+     * @return a new {@link OrganizationOptions} instance, used to customize how one or more {@link Organization}(s) are retrieved.
+     */
+
+    private static final Class<CreateOrganizationRequestBuilder> BUILDER_CLASS =
+            Classes.forName("com.stormpath.sdk.impl.organization.DefaultCreateOrganizationRequestBuilder");
+       
+    /**
+     * Returns a new {@link OrganizationOptions} instance, used to customize how one or more {@link Organization}(s) are retrieved.
+     *
+     * @return a new {@link OrganizationOptions} instance, used to customize how one or more {@link Organization}(s) are retrieved.
+     */
+    public static OrganizationOptions options() {
+        return (OrganizationOptions) Classes.newInstance("com.stormpath.sdk.impl.organization.DefaultOrganizationOptions");
+    }
+
+    /**
+     * Returns a new {@link OrganizationCriteria} instance to use to formulate an Organization query.
+     * <p/>
+     * Note that it is usually more common to use the {@link #where(com.stormpath.sdk.query.Criterion) where} method
+     * instead of this one as the {@code where} method usually lends to better readability.  For example:
+     * <pre>
+     * Organizations.criteria().add(Organizations.name().eqIgnoreCase("Foo"))...
+     * </pre>
+     * versus:
+     * <pre>
+     * Organizations.where(Organizations.name().eqIgnoreCase("Foo"))...
+     * </pre>
+     * or when using static imports:
+     * <pre>
+     * where(name().eqIgnoreCase("Foo"))...
+     * </pre>
+     * While all three statements are equivalent, the second and third examples are shorter and probably more readable.
+     *
+     * @return a new {@link OrganizationCriteria} instance to use to formulate an Organization query.
+     */
+    public static OrganizationCriteria criteria() {
+        return (OrganizationCriteria) Classes.newInstance("com.stormpath.sdk.impl.organization.DefaultOrganizationCriteria");
+    }
+
+    /**
+     * Creates a new {@link OrganizationCriteria} instance using the specified {@code criterion} as the first query condition.
+     *
+     * @return a new {@link OrganizationCriteria} instance using the specified {@code criterion} as the first query condition.
+     */
+    public static OrganizationCriteria where(Criterion criterion) {
+        return criteria().add(criterion);
+    }
+
+    /**
+     * Creates a new {@link StringExpressionFactory} instance reflecting the Organization {@link Organization#getName() name}
+     * property, to be used to construct a name Criterion when building an {@link OrganizationCriteria} query.  For example:
+     * <pre>
+     * Organizations.where(<b>Organizations.name()</b>.startsWithIgnoreCase("foo");
+     * </pre>
+     * The above example invokes the returned factory's <code>startsWithIgnoreCase("foo")</code> method.  This
+     * produces a name-specific {@link Criterion} which is added to the criteria query (via the
+     * {@link #where(com.stormpath.sdk.query.Criterion) where} method).  For example, the following code is equivalent:
+     * <pre>
+     * OrganizationCriteria criteria = Organizations.criteria();
+     * StringExpressionFactory nameExpressionFactory = Organizations.name();
+     * Criterion nameStartsWithFoo = nameExpressionFactory.startsWithIgnoreCase("foo");
+     * criteria.add(nameStartsWithFoo);
+     * </pre>
+     * The first code example is clearly more succinct and readable.
+     *
+     * @return a new {@link Organization#getName() name}-specific {@link StringExpressionFactory} instance, to be
+     *         used to construct a criterion when building an {@link OrganizationCriteria} query.
+     */
+    public static StringExpressionFactory name() {
+        return newStringExpressionFactory("name");
+    }
+
+    /**
+     * Creates a new {@link DateExpressionFactory} instance reflecting the Organization {@link Organization#getCreatedAt() createdAt}
+     * property, to be used to construct a createdAt Criterion when building an {@link OrganizationCriteria} query.  For example:
+     * <pre>
+     * Organizations.where(<b>Organizations.createdAt()</b>.matches("[,2014-04-05T12:00:00]");
+     * </pre>
+     * The above example invokes the returned factory's <code>matches("[,2014-04-05T12:00:00]"))</code> method.  This
+     * produces a name-specific {@link Criterion} which is added to the criteria query (via the
+     * {@link #where(Criterion) where} method).
+     * <pre>
+     * For example, the following code is equivalent:
+     * <pre>
+     * OrganizationCriteria criteria = Organizations.criteria();
+     * DateExpressionFactory createdAt = Organizations.createdAt();
+     * Criterion createdAtMatches = createdAt.matches("[,2014-04-05T12:00:00]");
+     * criteria.add(createdAtMatches);
+     * </pre>
+     *
+     * @return a new {@link Organization#getCreatedAt() createdAt}-specific {@link DateExpressionFactory} instance, to be
+     *         used to construct a criterion when building an {@link OrganizationCriteria} query.
+
+     */
+    public static DateExpressionFactory createdAt(){
+        return newDateExpressionFactory("createdAt");
+    }
+
+    /**
+     * Creates a new {@link DateExpressionFactory} instance reflecting the Organization {@link Organization#getModifiedAt() modifiedAt}
+     * property, to be used to construct a modifiedAt Criterion when building an {@link OrganizationCriteria} query.  For example:
+     * <pre>
+     * Organizations.where(<b>Organizations.modifiedAt()</b>.matches("[,2014-04-05T12:00:00]");
+     * </pre>
+     * The above example invokes the returned factory's <code>matches("[,2014-04-05T12:00:00]"))</code> method.  This
+     * produces a name-specific {@link Criterion} which is added to the criteria query (via the
+     * {@link #where(Criterion) where} method).
+     * <pre>
+     * For example, the following code is equivalent:
+     * <pre>
+     * OrganizationCriteria criteria = Organizations.criteria();
+     * DateExpressionFactory createdAt = Organizations.modifiedAt();
+     * Criterion modifiedAtMatches = modifiedAt.matches("[,2014-04-05T12:00:00]");
+     * criteria.add(modifiedAtMatches);
+     * </pre>
+     *
+     * @return a new {@link Organization#getModifiedAt() modifiedAt}-specific {@link DateExpressionFactory} instance, to be
+     *         used to construct a criterion when building an {@link OrganizationCriteria} query.
+     */
+    public static DateExpressionFactory modifiedAt(){
+        return newDateExpressionFactory("modifiedAt");
+    }
+
+    /**
+     * Creates a new {@link StringExpressionFactory} instance reflecting the Organization {@link Organization#getDescription() description}
+     * property, to be used to construct a description Criterion when building an {@link OrganizationCriteria} query. For example:
+     * <pre>
+     * Organizations.where(<b>Organizations.description()</b>.startsWithIgnoreCase("foo");
+     * </pre>
+     * The above example invokes the returned factory's <code>startsWithIgnoreCase("foo")</code> method.  This
+     * produces a description-specific {@link Criterion} which is added to the criteria query (via the
+     * {@link #where(com.stormpath.sdk.query.Criterion) where} method).  For example, the following code is equivalent:
+     * <pre>
+     * OrganizationCriteria criteria = Organizations.criteria();
+     * StringExpressionFactory descriptionExpressionFactory = Organizations.description();
+     * Criterion descriptionStartsWithFoo = descriptionExpressionFactory.startsWithIgnoreCase("foo");
+     * criteria.add(descriptionStartsWithFoo);
+     * </pre>
+     * The first code example is clearly more succinct and readable.
+     *
+     * @return a new {@link Organization#getDescription() description}-specific {@link StringExpressionFactory} instance, to be
+     *         used to construct a criterion when building an {@link OrganizationCriteria} query.
+     */
+    public static StringExpressionFactory description() {
+        return newStringExpressionFactory("description");
+    }
+
+    /**
+     * Creates a new {@link EqualsExpressionFactory} instance reflecting the Organization {@link Organization#getStatus() status}
+     * property, to be used to construct a status Criterion when building an {@link OrganizationCriteria} query.  For example:
+     * <pre>
+     * Organizations.where(<b>Directories.status()</b>.eq(DirectoryStatus.ENABLED);
+     * </pre>
+     * The above example invokes the returned factory's <code>eq()</code> method.  This
+     * produces a status-specific {@link Criterion} which is added to the criteria query (via the
+     * {@link #where(com.stormpath.sdk.query.Criterion) where} method).  For example, the following code is equivalent:
+     * <pre>
+     * OrganizationCriteria criteria = Organizations.criteria();
+     * StringExpressionFactory statusExpressionFactory = Organizations.status();
+     * Criterion statusEqualsEnabled = statusExpressionFactory.eq(DirectoryStatus.ENABLED);
+     * criteria.add(statusEqualsEnabled);
+     * </pre>
+     * The first code example is clearly more succinct and readable.
+     *
+     * @return a new {@link Organization#getStatus() status}-specific {@link StringExpressionFactory} instance, to be
+     *         used to construct a criterion when building an {@link OrganizationCriteria} query.
+     */
+    public static EqualsExpressionFactory status() {
+        return newEqualsExpressionFactory("status");
+    }
+
+    public static CreateOrganizationRequestBuilder newCreateRequestFor(Organization organization) {
+        Constructor ctor = Classes.getConstructor(BUILDER_CLASS, Organization.class);
+        return (CreateOrganizationRequestBuilder) Classes.instantiate(ctor, organization);
+    }
+
+    private static StringExpressionFactory newStringExpressionFactory(String propName) {
+        final String FQCN = "com.stormpath.sdk.impl.query.DefaultStringExpressionFactory";
+        return (StringExpressionFactory) Classes.newInstance(FQCN, propName);
+    }
+
+    private static DateExpressionFactory newDateExpressionFactory(String propName) {
+        final String FQCN = "com.stormpath.sdk.impl.query.DefaultDateExpressionFactory";
+        return (DateExpressionFactory) Classes.newInstance(FQCN, propName);
+    }
+
+    private static EqualsExpressionFactory newEqualsExpressionFactory(String propName) {
+        final String FQCN = "com.stormpath.sdk.impl.query.DefaultEqualsExpressionFactory";
+        return (EqualsExpressionFactory) Classes.newInstance(FQCN, propName);
+    }
+}
--- impl/src/main/java/com/stormpath/sdk/impl/organization/DefaultOrganization.java
+++ impl/src/main/java/com/stormpath/sdk/impl/organization/DefaultOrganization.java
@@ -15,11 +15,20 @@
 */
 package com.stormpath.sdk.impl.organization;
 
-import com.stormpath.sdk.directory.AccountStoreVisitor;
+import com.stormpath.sdk.account.*;
+import com.stormpath.sdk.directory.*;
+import com.stormpath.sdk.group.*;
 import com.stormpath.sdk.impl.ds.InternalDataStore;
-import com.stormpath.sdk.impl.resource.*;
-import com.stormpath.sdk.organization.Organization;
-import com.stormpath.sdk.organization.OrganizationStatus;
+import com.stormpath.sdk.impl.resource.AbstractExtendableInstanceResource;
+import com.stormpath.sdk.impl.resource.StatusProperty;
+import com.stormpath.sdk.impl.resource.StringProperty;
+import com.stormpath.sdk.impl.resource.ResourceReference;
+import com.stormpath.sdk.impl.resource.CollectionReference;
+import com.stormpath.sdk.impl.resource.Property;
+import com.stormpath.sdk.lang.Assert;
+import com.stormpath.sdk.organization.*;
+import com.stormpath.sdk.query.Criteria;
+import com.stormpath.sdk.resource.ResourceException;
 import com.stormpath.sdk.tenant.Tenant;
 
 import java.util.Map;
@@ -32,13 +41,29 @@ public class DefaultOrganization extends AbstractExtendableInstanceResource impl
     // SIMPLE PROPERTIES
     static final StringProperty NAME = new StringProperty("name");
     static final StringProperty DESCRIPTION = new StringProperty("description");
+    static final StringProperty NAME_KEY = new StringProperty("nameKey");
     static final StatusProperty<OrganizationStatus> STATUS = new StatusProperty<OrganizationStatus>(OrganizationStatus.class);
 
     // INSTANCE RESOURCE REFERENCES:
     static final ResourceReference<Tenant> TENANT = new ResourceReference<Tenant>("tenant", Tenant.class);
+    static final ResourceReference<OrganizationAccountStoreMapping> DEFAULT_ACCOUNT_STORE_MAPPING =
+            new ResourceReference<OrganizationAccountStoreMapping>("defaultAccountStoreMapping", OrganizationAccountStoreMapping.class);
+    static final ResourceReference<OrganizationAccountStoreMapping> DEFAULT_GROUP_STORE_MAPPING   =
+            new ResourceReference<OrganizationAccountStoreMapping>("defaultGroupStoreMapping", OrganizationAccountStoreMapping.class);
+
+    // COLLECTION RESOURCE REFERENCES:
+    static final CollectionReference<AccountList, Account> ACCOUNTS =
+            new CollectionReference<AccountList, Account>("accounts", AccountList.class, Account.class);
+    static final CollectionReference<DirectoryList, Directory> DIRECTORIES =
+            new CollectionReference<DirectoryList, Directory>("directories", DirectoryList.class, Directory.class);
+    static final CollectionReference<GroupList, Group> GROUPS =
+            new CollectionReference<GroupList, Group>("groups", GroupList.class, Group.class);
+    static final CollectionReference<OrganizationAccountStoreMappingList, OrganizationAccountStoreMapping> ACCOUNT_STORE_MAPPINGS =
+            new CollectionReference<OrganizationAccountStoreMappingList, OrganizationAccountStoreMapping>("organizationAccountStoreMappings", OrganizationAccountStoreMappingList.class, OrganizationAccountStoreMapping.class);
+
 
     private static final Map<String, Property> PROPERTY_DESCRIPTORS = createPropertyDescriptorMap(
-            NAME, DESCRIPTION, STATUS, TENANT, CUSTOM_DATA);
+            NAME, DESCRIPTION, NAME_KEY, STATUS, TENANT, CUSTOM_DATA, DIRECTORIES, GROUPS, DEFAULT_ACCOUNT_STORE_MAPPING, DEFAULT_GROUP_STORE_MAPPING, ACCOUNT_STORE_MAPPINGS, ACCOUNTS);
 
     public DefaultOrganization(InternalDataStore dataStore) {
         super(dataStore);
@@ -61,6 +86,17 @@ public class DefaultOrganization extends AbstractExtendableInstanceResource impl
     @Override
     public Organization setName(String name) {
         setProperty(NAME, name);
+        return this;
+    }
+
+    @Override
+    public String getNameKey() {
+        return getString(NAME_KEY);
+    }
+
+    @Override
+    public Organization setNameKey(String nameKey) {
+        setProperty(NAME_KEY, nameKey);
         return this;
     }
 
@@ -95,4 +131,202 @@ public class DefaultOrganization extends AbstractExtendableInstanceResource impl
         getDataStore().delete(this);
     }
 
+    @Override
+    public GroupList getGroups() {
+        return getResourceProperty(GROUPS);
+    }
+
+    @Override
+    public GroupList getGroups(Map<String, Object> queryParams) {
+        GroupList list = getGroups(); // obtains the href: no query is executed until iteration occurs
+        return getDataStore().getResource(list.getHref(), GroupList.class, queryParams);
+    }
+
+    @Override
+    public GroupList getGroups(GroupCriteria criteria) {
+        GroupList list = getGroups(); // obtains the href: no query is executed until iteration occurs
+        return getDataStore().getResource(list.getHref(), GroupList.class, (Criteria<GroupCriteria>) criteria);
+    }
+
+    @Override
+    public DirectoryList getDirectories() {
+        return getResourceProperty(DIRECTORIES);
+    }
+
+    @Override
+    public DirectoryList getDirectories(Map<String, Object> queryParams) {
+        DirectoryList list = getDirectories(); // obtains the href: no query is executed until iteration occurs
+        return getDataStore().getResource(list.getHref(), DirectoryList.class, queryParams);
+    }
+
+    @Override
+    public DirectoryList getDirectories(DirectoryCriteria criteria) {
+        DirectoryList list = getDirectories(); // obtains the href: no query is executed until iteration occurs
+        return getDataStore().getResource(list.getHref(), DirectoryList.class, (Criteria<DirectoryCriteria>) criteria);
+    }
+
+    @Override
+    public OrganizationAccountStoreMappingList getOrganizationAccountStoreMappings() {
+        return getResourceProperty(ACCOUNT_STORE_MAPPINGS);
+    }
+
+    @Override
+    public OrganizationAccountStoreMappingList getOrganizationAccountStoreMappings(Map<String, Object> queryParams) {
+        OrganizationAccountStoreMappingList accountStoreMappings =
+                getOrganizationAccountStoreMappings(); //obtains the href: does not execute a query until iteration occurs
+        return getDataStore().getResource(accountStoreMappings.getHref(), OrganizationAccountStoreMappingList.class, queryParams);
+    }
+    
+    @Override
+    public AccountStore getDefaultAccountStore() {
+        OrganizationAccountStoreMapping accountStoreMap = getResourceProperty(DEFAULT_ACCOUNT_STORE_MAPPING);
+        return accountStoreMap == null ? null : accountStoreMap.getAccountStore();
+    }
+
+    @Override
+    public void setDefaultAccountStore(AccountStore accountStore) {
+        OrganizationAccountStoreMappingList accountStoreMappingList = getOrganizationAccountStoreMappings();
+        boolean needToCreateNewStore = true;
+        for (OrganizationAccountStoreMapping accountStoreMapping : accountStoreMappingList) {
+            if (accountStoreMapping.getAccountStore().getHref().equals(accountStore.getHref())) {
+                needToCreateNewStore = false;
+                accountStoreMapping.setDefaultAccountStore(true);
+                accountStoreMapping.save();
+                setProperty(DEFAULT_ACCOUNT_STORE_MAPPING, accountStoreMapping);
+                break;
+            }
+        }
+        if (needToCreateNewStore) {
+            OrganizationAccountStoreMapping mapping = addAccountStore(accountStore);
+            mapping.setDefaultAccountStore(true);
+            mapping.save();
+            setProperty(DEFAULT_ACCOUNT_STORE_MAPPING, mapping);
+        }
+        save();
+    }
+
+    @Override
+    public OrganizationAccountStoreMappingList getOrganizationAccountStoreMappings(OrganizationAccountStoreMappingCriteria criteria) {
+        return getResourceProperty(ACCOUNT_STORE_MAPPINGS);
+    }
+
+    @Override
+    public AccountStore getDefaultGroupStore() {
+        OrganizationAccountStoreMapping accountStoreMap = getResourceProperty(DEFAULT_GROUP_STORE_MAPPING);
+        return accountStoreMap == null ? null : accountStoreMap.getAccountStore();
+    }
+
+    @Override
+    public void setDefaultGroupStore(AccountStore accountStore) {
+        OrganizationAccountStoreMappingList accountStoreMappingList = getOrganizationAccountStoreMappings();
+        boolean needToCreateNewStore = true;
+        for (OrganizationAccountStoreMapping accountStoreMapping : accountStoreMappingList) {
+            if (accountStoreMapping.getAccountStore().getHref().equals(accountStore.getHref())) {
+                needToCreateNewStore = false;
+                accountStoreMapping.setDefaultGroupStore(true);
+                accountStoreMapping.save();
+                setProperty(DEFAULT_GROUP_STORE_MAPPING, accountStoreMapping);
+                break;
+            }
+        }
+        if (needToCreateNewStore) {
+            OrganizationAccountStoreMapping mapping = addAccountStore(accountStore);
+            mapping.setDefaultGroupStore(true);
+            mapping.save();
+            setProperty(DEFAULT_GROUP_STORE_MAPPING, mapping);
+        }
+        save();
+    }
+
+    @Override
+    public OrganizationAccountStoreMapping createOrganizationAccountStoreMapping(OrganizationAccountStoreMapping mapping) throws ResourceException {
+        return getDataStore().create("/" + ACCOUNT_STORE_MAPPINGS.getName(), mapping);
+    }
+
+    @Override
+    public OrganizationAccountStoreMapping addAccountStore(AccountStore accountStore) throws ResourceException {
+        Assert.notNull(accountStore, "accountStore cannot be null.");
+        OrganizationAccountStoreMapping accountStoreMapping = getDataStore().instantiate(OrganizationAccountStoreMapping.class);
+        accountStoreMapping.setAccountStore(accountStore);
+        accountStoreMapping.setOrganization(this);
+        accountStoreMapping.setListIndex(Integer.MAX_VALUE);
+        return createOrganizationAccountStoreMapping(accountStoreMapping);
+    }
+
+    @Override
+    public Group createGroup(Group group) throws ResourceException {
+        Assert.notNull(group, "Group instance cannot be null.");
+        CreateGroupRequest request = Groups.newCreateRequestFor(group).build();
+        return createGroup(request);
+    }
+
+    @Override
+    public Tenant getTenant() {
+        return getResourceProperty(TENANT);
+    }
+
+    @Override
+    public Group createGroup(CreateGroupRequest request) {
+        Assert.notNull(request, "Request cannot be null.");
+
+        final Group group = request.getGroup();
+        String href = getGroups().getHref();
+
+        if (request.isGroupOptionsSpecified()) {
+            return getDataStore().create(href, group, request.getGroupOptions());
+        }
+        return getDataStore().create(href, group);
+    }
+
+    public Account createAccount(Account account) {
+        Assert.notNull(account, "Account instance cannot be null.");
+        CreateAccountRequest request = Accounts.newCreateRequestFor(account).build();
+        return createAccount(request);
+    }
+
+    @Override
+    public AccountList getAccounts() {
+        return getResourceProperty(ACCOUNTS);
+    }
+
+    @Override
+    public AccountList getAccounts(Map<String, Object> queryParams) {
+        AccountList list = getAccounts(); //safe to get the href: does not execute a query until iteration occurs
+        return getDataStore().getResource(list.getHref(), AccountList.class, queryParams);
+    }
+
+    @Override
+    public AccountList getAccounts(AccountCriteria criteria) {
+        AccountList list = getAccounts();  //safe to get the href: does not execute a query until iteration occurs
+        return getDataStore().getResource(list.getHref(), AccountList.class, (Criteria<AccountCriteria>) criteria);
+    }
+
+    @Override
+    public Account createAccount(CreateAccountRequest request) {
+        Assert.notNull(request, "Request cannot be null.");
+        final Account account = request.getAccount();
+        String href = getAccounts().getHref();
+
+        char querySeparator = '?';
+
+        if (request.isRegistrationWorkflowOptionSpecified()) {
+            href += querySeparator + "registrationWorkflowEnabled=" + request.isRegistrationWorkflowEnabled();
+            querySeparator = '&';
+        }
+
+        if (request.isPasswordFormatSpecified()) {
+            href += querySeparator + "passwordFormat=" + request.getPasswordFormat();
+        }
+
+        if (request.isAccountOptionsSpecified()) {
+            return getDataStore().create(href, account, request.getAccountOptions());
+        }
+
+        return getDataStore().create(href, account);
+    }
+
+    @Override
+    public void accept(AccountStoreVisitor visitor) {
+        visitor.visit(this);
+    }
 }
