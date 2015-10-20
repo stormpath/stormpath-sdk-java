1cf847897fba5510e8134079a0bc43eaf6c95be5
Anai Arroyo <anai.arroyo@stormpath.com>
2015-10-20 10:09:32 -0600

    Changes from Code Review

--- api/src/main/java/com/stormpath/sdk/accountStoreMapping/AccountStoreHolder.java
+++ api/src/main/java/com/stormpath/sdk/accountStoreMapping/AccountStoreHolder.java
@@ -15,6 +15,8 @@
 */
 package com.stormpath.sdk.accountStoreMapping;
 
+import com.stormpath.sdk.directory.AccountStore;
+
 /**
  * Interface to be implemented by {@link com.stormpath.sdk.resource.Resource Resources} capable of storing {@link com.stormpath.sdk.directory.AccountStore account stores}. For example:
  * {@link com.stormpath.sdk.application.Application Applications} and {@link com.stormpath.sdk.organization.Organization Organizations}.
@@ -22,4 +24,110 @@ package com.stormpath.sdk.accountStoreMapping;
  * @since 1.0.RC5.1
  */
 public interface AccountStoreHolder<T extends AccountStoreHolder> {
+
+    /**
+     * Returns the {@link AccountStore} (either a {@link com.stormpath.sdk.group.Group} or a
+     * {@link com.stormpath.sdk.directory.Directory Directory}) used to persist
+     * new accounts created by the Application or Directory, or
+     * {@code null} if no accountStore has been designated.
+     * <p/>
+     * Because Applications and Directories are not {@code AccountStore}s, they delegate to a Group or Directory
+     * when creating accounts; this method returns the AccountStore to which the Application or Organization delegate
+     * new account persistence.
+     * <h3>Directory or Group?</h3>
+     * As both Group and Directory are sub-interfaces of {@link AccountStore}, you can determine which of the two
+     * is returned by using the <a href="http://en.wikipedia.org/wiki/Visitor_pattern">Visitor design pattern</a>.  For
+     * example:
+     * <p/>
+     * <pre>
+     * AccountStore accountStore = application.getDefaultAccountStore();
+     * accountStore.accept(new {@link com.stormpath.sdk.directory.AccountStoreVisitor AccountStoreVisitor}() {
+     *
+     *     public void visit(Directory directory) {
+     *         //the accountStore is a Directory
+     *     }
+     *
+     *     public void visit(Group group) {
+     *         //the accountStore is a Group;
+     *     }
+     * };
+     * </pre>
+     *
+     * @return the {@link AccountStore} (which will be either a Group or Directory) used to persist
+     *         new accounts created by the Account Store Holder (Application or Organization), or
+     *         {@code null} if no accountStore has been designated.
+     * @since 0.9
+     */
+    AccountStore getDefaultAccountStore();
+
+    /**
+     * Sets the {@link AccountStore} (either a {@link com.stormpath.sdk.group.Group} or a
+     * {@link com.stormpath.sdk.directory.Directory Directory}) used to persist
+     * new accounts created by the Application or Organization.
+     * <p/>
+     * Because Applications and Directories are not {@code AccountStore}s, they delegate to a Group or Directory
+     * when creating accounts; this method returns the AccountStore to which the Application or Organization delegate
+     * new account persistence.
+     * </p>
+     *
+     * @param accountStore the {@link AccountStore} (which will be either a Group or Directory) used to persist
+     *                     new accounts created by the Application or Directory
+     */
+    void setDefaultAccountStore(AccountStore accountStore);
+
+    /**
+     * Returns the {@link AccountStore} used to persist
+     * new groups created by the Application or Organization, or
+     * {@code null} if no accountStore has been designated. <b>Stormpath's current REST API requires this to be
+     * a Directory. However, this could be a Group in the future, so do not assume it is always a
+     * Directory if you want your code to be function correctly if/when this support is added.</b>  Avoid casting the
+     * returned value directly to a Directory: use the Visitor pattern as explained below.
+     * <p/>
+     * Because Applications and Directories are not {@code AccountStore}s, they delegate to a Group or Directory
+     * when creating accounts; this method returns the AccountStore to which the Application or Organization delegate
+     * new account persistence.
+     * <h3>Directory or Group?</h3>
+     * As both Group and Directory are sub-interfaces of {@link AccountStore}, you can determine which of the two
+     * is returned by using the <a href="http://en.wikipedia.org/wiki/Visitor_pattern">Visitor design pattern</a>.  For
+     * example:
+     * <p/>
+     * <pre>
+     * AccountStore groupStore = organization.getDefaultGroupStore();
+     * groupStore.accept(new {@link com.stormpath.sdk.directory.AccountStoreVisitor AccountStoreVisitor}() {
+     *
+     *     public void visit(Directory directory) {
+     *         //groupStore is a Directory
+     *     }
+     *
+     *     public void visit(Group group) {
+     *         //groupStore is a Group;
+     *     }
+     * };
+     * </pre>
+     * Again, in practice, Stormpath's current REST API requires this to be a Directory.  However, this could be
+     * a Group in the future, so do not assume it will always be a Directory if you want your code to be
+     * forward compatible; use the Visitor pattern and do not cast directly to a Directory.
+     *
+     * @return the {@link AccountStore} (which will be either a Group or Directory) used to persist
+     *         new groups created by the Application or Organization, or
+     *         {@code null} if no accountStore has been designated.
+     * @since 0.9
+     */
+    AccountStore getDefaultGroupStore();
+
+    /**
+     * Sets the {@link AccountStore} (a {@link com.stormpath.sdk.directory.Directory Directory}) that will be used to
+     * persist new groups created by the Application or Organization.
+     * <b>Stormpath's current REST API requires this to be
+     * a Directory. However, this could be a Group in the future, so do not assume it is always a
+     * Directory if you want your code to be function correctly if/when this support is added.</b>
+     * <p/>
+     * Because Applications and Directories are not {@code AccountStore}s, they delegate to a Group or Directory
+     * when creating accounts; this method returns the AccountStore to which the Application or Organization delegate
+     * new account persistence.
+     *
+     * @param accountStore the {@link AccountStore} (which will be either a Group or Directory) used to persist
+     *                     new groups created by the Application or Organization.
+     */
+    void setDefaultGroupStore(AccountStore accountStore);
 }
