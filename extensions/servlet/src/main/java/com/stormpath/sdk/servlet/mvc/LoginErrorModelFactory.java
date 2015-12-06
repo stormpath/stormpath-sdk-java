23ee84a8cbf5cf5c14a132d130f17e4dbb728c76
Micah Silverman <micah@afitnerd.com>
2015-12-06 11:10:38 -0500

    Refactored LoginErrorModelFactory so that it only adds an error if there was an exception.

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/LoginErrorModelFactory.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/LoginErrorModelFactory.java
@@ -18,9 +18,12 @@ public class LoginErrorModelFactory implements ErrorModelFactory {
     private static final Logger log = LoggerFactory.getLogger(LoginErrorModelFactory.class);
 
     public List<String> toErrors(HttpServletRequest request, Form form, Exception e) {
-        log.debug("Unable to login user.", e);
-        List<String> errors = new ArrayList<String>(1);
-        errors.add("Invalid username or password.");
-        return errors;
+        if (e != null) {
+            log.debug("Unable to login user.", e);
+            List<String> errors = new ArrayList<String>(1);
+            errors.add("Invalid username or password.");
+            return errors;
+        }
+        return null;
     }
 }
