2f3f54fec9af2ffee5ca54950745bde2abe8cce4
Micah Silverman <micah@afitnerd.com>
2015-12-09 17:28:04 -0500

    Made changes per @mrioan. Ensure that ID Site result callback path is permitted for Spring Security.

--- extensions/spring/boot/stormpath-spring-security-webmvc-spring-boot-starter/src/main/java/com/stormpath/spring/boot/autoconfigure/StormpathWebSecurityAutoConfiguration.java
+++ extensions/spring/boot/stormpath-spring-security-webmvc-spring-boot-starter/src/main/java/com/stormpath/spring/boot/autoconfigure/StormpathWebSecurityAutoConfiguration.java
@@ -31,7 +31,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.Filter;
@@ -89,6 +88,7 @@ public class StormpathWebSecurityAutoConfiguration extends AbstractStormpathWebS
 
     @Bean
     @ConditionalOnMissingBean(name="springSecurityIdSiteResultListener")
+    @ConditionalOnProperty(name="stormpath.web.idSite.enabled")
     public IdSiteResultListener springSecurityIdSiteResultListener() {
         return super.springSecurityIdSiteResultListener();
     }
