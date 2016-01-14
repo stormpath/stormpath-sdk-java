d66e32ae5828e864c150bff6aa7c604f303a97cb
Anai Arroyo <anai.arroyo@stormpath.com>
2016-01-13 18:11:50 -0600

    #164 Cache does not return correctly resources with custom data or expansions

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/application/ApplicationIT.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/application/ApplicationIT.groovy
@@ -617,6 +617,43 @@ class ApplicationIT extends ClientIT {
     }
 
     /**
+     * Test created to demonstrate cache problems related to expansions. In this case, the expanded resource is cached
+     * without the expansion data and when requested with an expansion, it will be retrieved from the cache, producing an error.
+     * This tests fails only if the following line added to the WriteCacheFilter class as a workaround is removed or commented out:
+     *
+     * (!request.getUri().hasQuery() || !request.getUri().getQuery().containsKey("expand") .....
+     *
+     * @since 1.0.RC9
+     */
+    @Test(enabled = false)
+    void testExpansionNotWorkingBecauseOfCache(){
+
+        def app = createTempApp()
+
+        def count = 2
+        while (count >= 0) {
+            def account = createTestAccount(client, app)
+            deleteOnTeardown(account)
+            count --
+        }
+
+        def appList = client.getApplications(Applications.where(Applications.name().eqIgnoreCase(app.name)))
+        def retrieved = appList.iterator().next()
+
+        Map properties = getValue(AbstractResource, retrieved, "properties")
+        assertEquals properties.get("accounts").get("items"), null
+
+        retrieved = client.getApplications(Applications.where(Applications.name().eqIgnoreCase(app.name)).withAccounts())
+        retrieved = appList.iterator().next()
+
+        properties = getValue(AbstractResource, retrieved, "properties")
+        def items = properties.get("accounts").get("items")
+        if (items == null){
+            fail("Test fails because the application was retrieved seconds ago and was kept in the cache. When trying to load an expanded collection, the resource from the cache is returned, which is an error.")
+        }
+    }
+
+    /**
      * @since 1.0.RC8
      */
     @Test
