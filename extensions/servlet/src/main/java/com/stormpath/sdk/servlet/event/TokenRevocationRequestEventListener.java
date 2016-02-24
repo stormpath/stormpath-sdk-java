e285fb5654433785199c898a9e815ea4f064275d
Mario <mario@stormpath.com>
2016-02-24 16:47:07 -0300

    #479 minor improvements to messages.

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/event/TokenRevocationRequestEventListener.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/event/TokenRevocationRequestEventListener.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2016 Stormpath, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2016 Stormpath, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stormpath.sdk.servlet.event;
 
 import com.stormpath.sdk.client.Client;
@@ -40,8 +40,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
-* @since 1.0.RC8.3
-*/
+ * @since 1.0.RC8.3
+ */
 public class TokenRevocationRequestEventListener implements RequestEventListener {
 
     private static final Logger log = LoggerFactory.getLogger(TokenRevocationRequestEventListener.class);
@@ -94,7 +94,7 @@ public class TokenRevocationRequestEventListener implements RequestEventListener
             //obtaining a new access token
         }
         if (log.isDebugEnabled()) {
-            log.debug("The current access and refresh token for {} has been revoked.", e.getAccount().getEmail());
+            log.debug("The current access and refresh tokens for {} have been revoked.", e.getAccount().getEmail());
         }
     }
 
@@ -110,10 +110,8 @@ public class TokenRevocationRequestEventListener implements RequestEventListener
             AccessToken accessToken = ((DefaultDataStore)client.getDataStore()).instantiate(AccessToken.class, map, null, true);
             accessToken.delete();
         } catch (ResourceException e) {
-            //Let's prevent an error to avoid the flow to continue (failing to try to delete an access token for example)
-            if (log.isDebugEnabled()) {
-                log.debug("There was an error trying to delete access token with ID " + accessTokenId + ". Exception was: " + e.getStackTrace());
-            }
+            //Let's prevent an error to avoid the flow to continue
+            log.error("There was an error trying to delete access token with ID " + accessTokenId + ". Exception was: " + e.getStackTrace());
         }
     }
 
@@ -126,10 +124,8 @@ public class TokenRevocationRequestEventListener implements RequestEventListener
             refreshToken.delete();
         } catch (ResourceException e) {
             //Let's prevent an error to avoid the flow to continue, this component is basically a listener that tries to delete
-            //access tokens on logout, we will only post this error in the log
-            if (log.isDebugEnabled()) {
-                log.debug("There was an error trying to delete refresh token with ID " + refreshTokenId + ". Exception was: " + e.getStackTrace());
-            }
+            //the current access and refresh tokens on logout, we will only post this error in the log
+            log.error("There was an error trying to delete refresh token with ID " + refreshTokenId + ". Exception was: " + e.getStackTrace());
         }
     }
 }
