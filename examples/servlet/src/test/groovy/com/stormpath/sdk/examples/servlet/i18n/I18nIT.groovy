fce2400c8505960823bc5f326a6f61cd090fcc26
Julio Arias <jarias01@gmail.com>
2016-03-11 09:23:06 -0600

    #500 Latest code review changes
    - Added tests for default locale
    - All examples depend on the examples root pom to share config between them
    - So improve in-code comments

--- examples/servlet/src/test/groovy/com/stormpath/sdk/examples/servlet/i18n/I18nIT.groovy
+++ examples/servlet/src/test/groovy/com/stormpath/sdk/examples/servlet/i18n/I18nIT.groovy
@@ -1,16 +1,42 @@
 package com.stormpath.sdk.examples.servlet.i18n
 
-import com.jayway.restassured.RestAssured
-import com.jayway.restassured.config.DecoderConfig
 import org.testng.annotations.Test
 
 import static com.jayway.restassured.RestAssured.given
 import static org.hamcrest.Matchers.equalTo
 
 /**
- *
+ * @since 1.0.RC9
  */
 class I18nIT {
+
+    @Test
+    void testLoginPageDefaultLocale() {
+        given()
+                .when()
+                .get("http://localhost:8080/login")
+                .then()
+                .body(
+                "html.head.title", equalTo("Login"),
+                "**.findAll { it.@class == 'header' }.span.a", equalTo("Create Account"),
+                "**.findAll { it.@type == 'submit' }", equalTo("Login"),
+                "**.findAll { it.@class == 'to-login' }", equalTo("Forgot Password?")
+        )
+    }
+
+    @Test
+    void testRegisterPageDefaultLocale() {
+        given()
+                .when()
+                .get("http://localhost:8080/register")
+                .then()
+                .body(
+                "html.head.title", equalTo("Create Account"),
+                "**.findAll { it.@type == 'submit' }", equalTo("Create Account"),
+                "**.findAll { it.@class == 'to-login' }", equalTo("Back to Login")
+        )
+    }
+
     @Test
     void testLoginPage() {
         given()
