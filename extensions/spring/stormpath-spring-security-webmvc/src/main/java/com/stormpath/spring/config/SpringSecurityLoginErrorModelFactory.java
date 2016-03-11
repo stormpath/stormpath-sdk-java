d78217d6d3c30718cd12c861d1e90430626bc883
Miguel Zumbado <miguel.zumbado@edify.cr>
2016-03-11 16:49:23 -0600

    396 getting the messageSource from the context

--- extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/config/SpringSecurityLoginErrorModelFactory.java
+++ extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/config/SpringSecurityLoginErrorModelFactory.java
@@ -2,14 +2,13 @@ package com.stormpath.spring.config;
 
 import com.stormpath.sdk.lang.Strings;
 import com.stormpath.sdk.servlet.form.Form;
-import com.stormpath.sdk.servlet.i18n.DefaultMessageSource;
 import com.stormpath.sdk.servlet.i18n.MessageSource;
 import com.stormpath.sdk.servlet.mvc.ErrorModelFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * @since 1.0.RC7
@@ -18,19 +17,21 @@ public class SpringSecurityLoginErrorModelFactory implements ErrorModelFactory {
 
     private static final String  INVALID_LOGIN_MESSAGE = "stormpath.web.login.form.errors.invalidLogin";
 
+    @Autowired
+    MessageSource messageSource;
+
     @Override
     public List<String> toErrors(HttpServletRequest request, Form form, Exception exception) {
 
         String query = Strings.clean(request.getQueryString());
         if (query != null && query.contains("error")) {
-            return Collections.singletonList(getInvalidLoginMessage(request.getLocale()));
+            return Collections.singletonList(getInvalidLoginMessage(request));
         }
 
         return null;
     }
 
-    private String getInvalidLoginMessage(Locale locale) {
-        MessageSource messageSource = new DefaultMessageSource();
-        return messageSource.getMessage(INVALID_LOGIN_MESSAGE, locale);
+    private String getInvalidLoginMessage(HttpServletRequest request) {
+        return messageSource.getMessage(INVALID_LOGIN_MESSAGE, request.getLocale());
     }
 }
