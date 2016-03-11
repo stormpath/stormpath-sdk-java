828613ec710b98f3f65de0384026cc47267f206f
Miguel Zumbado <miguel.zumbado@edify.cr>
2016-03-11 14:35:39 -0600

    396 adding the validation to spring sec

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/LoginErrorModelFactory.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/LoginErrorModelFactory.java
@@ -1,12 +1,15 @@
 package com.stormpath.sdk.servlet.mvc;
 
 import com.stormpath.sdk.servlet.form.Form;
+import com.stormpath.sdk.servlet.i18n.DefaultMessageSource;
+import com.stormpath.sdk.servlet.i18n.MessageSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Generates the error model on a failed login attempt.
@@ -16,14 +19,20 @@ import java.util.List;
 public class LoginErrorModelFactory implements ErrorModelFactory {
 
     private static final Logger log = LoggerFactory.getLogger(LoginErrorModelFactory.class);
+    private static final String  INVALID_LOGIN_MESSAGE = "stormpath.web.login.form.errors.invalidLogin";
 
     public List<String> toErrors(HttpServletRequest request, Form form, Exception e) {
         if (e != null) {
             log.debug("Unable to login user.", e);
             List<String> errors = new ArrayList<String>(1);
-            errors.add("Invalid username or password.");
+            errors.add(getInvalidLoginMessage(request.getLocale()));
             return errors;
         }
         return null;
+    }
+
+    private String getInvalidLoginMessage(Locale locale) {
+        MessageSource messageSource = new DefaultMessageSource();
+        return messageSource.getMessage(INVALID_LOGIN_MESSAGE, locale);
     }
 }
