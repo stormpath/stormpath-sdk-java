f7be26d815a21d0120d9d5c95d88227853d2ffe0
Matt Raible <matt@raibledesigns.com>
2016-04-27 16:34:23 -0600

    Remove on-the-fly stormpath.properties creation since tests fail when running 'mvn install' from top-level

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/client/DefaultClientBuilderTest.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/client/DefaultClientBuilderTest.groovy
@@ -4,8 +4,6 @@ import com.stormpath.sdk.client.AuthenticationScheme
 import com.stormpath.sdk.client.Clients
 import com.stormpath.sdk.impl.cache.DefaultCache
 import com.stormpath.sdk.lang.Duration
-import org.testng.annotations.AfterClass
-import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
@@ -17,35 +15,6 @@ import static org.testng.Assert.assertTrue
 class DefaultClientBuilderTest {
 
     def builder, client
-    File propsFile
-
-    @BeforeClass
-    void setup() {
-        Properties props = new Properties()
-        props.setProperty('client.apiKey.file', 'classpath:key.txt')
-        props.setProperty('client.apiKey.id', '12')
-        props.setProperty('client.apiKey.secret', '13')
-        props.setProperty('client.cacheManager.defaultTtl', '10000')
-        props.setProperty('client.cacheManager.defaultTti', '10000')
-        props.setProperty('client.cacheManager.caches.com.stormpath.sdk.impl.client.Account.tti', '1000')
-        props.setProperty('client.cacheManager.caches.com.stormpath.sdk.impl.client.Account.ttl', '1500')
-        props.setProperty('client.baseUrl', 'https://api.stormpath.com/v42')
-        props.setProperty('client.connectionTimeout', '10')
-        props.setProperty('client.authenticationScheme', 'BASIC')
-        props.setProperty('client.proxy.port', '9000')
-        props.setProperty('client.proxy.host', 'localhost')
-        props.setProperty('client.proxy.username', 'foo')
-        props.setProperty('client.proxy.password', 'bar')
-        props.setProperty('application.name', 'appName')
-        props.setProperty('application.href', 'appHref')
-        propsFile = new File('src/test/resources/stormpath.properties')
-        props.store(propsFile.newWriter(), null)
-    }
-
-    @AfterClass
-    public void clean() {
-        propsFile.delete()
-    }
 
     @BeforeMethod
     void before() {
