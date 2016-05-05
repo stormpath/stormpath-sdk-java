52bda298c8fea5288b368551ccceec013ff0e9dc
Matt Raible <matt@raibledesigns.com>
2016-05-05 17:57:25 -0600

    Test to compare the properties in com/stormpath/sdk/config/stormpath.properties with the ones defined in additional-spring-configuration-metadata.json

--- extensions/spring/boot/stormpath-spring-boot-starter/src/test/groovy/com/stormpath/spring/boot/config/SpringMetadataVersusCorePropertiesTest.groovy
+++ extensions/spring/boot/stormpath-spring-boot-starter/src/test/groovy/com/stormpath/spring/boot/config/SpringMetadataVersusCorePropertiesTest.groovy
@@ -1,5 +1,15 @@
 package com.stormpath.spring.boot.config
 
+import com.stormpath.sdk.impl.config.JSONPropertiesSource
+import com.stormpath.sdk.impl.config.ResourcePropertiesSource
+import com.stormpath.sdk.impl.io.ClasspathResource
+import com.stormpath.sdk.impl.io.DefaultResourceFactory
+import com.stormpath.sdk.impl.io.Resource
+import com.stormpath.sdk.impl.io.ResourceFactory
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+
+import static org.testng.Assert.assertEquals
 /**
  * A test that compares the properties in com/stormpath/sdk/config/stormpath.properties
  * with the ones defined in additional-spring-configuration-metadata.json.
@@ -7,5 +17,43 @@ package com.stormpath.spring.boot.config
  * @since 1.0.0
  */
 class SpringMetadataVersusCorePropertiesTest {
+    def METADATA_CONFIG_LOCATION = ClasspathResource.SCHEME_PREFIX + "META-INF/additional-spring-configuration-metadata.json"
+    def DEFAULT_CONFIG_LOCATION = ClasspathResource.SCHEME_PREFIX + "com/stormpath/sdk/config/stormpath.properties"
+    def metadataProperties, defaultProperties
+    def metadataKeys = []
 
+    @BeforeClass
+    void before() {
+        ResourceFactory resourceFactory = new DefaultResourceFactory()
+        Resource metadataConfig = resourceFactory.createResource(METADATA_CONFIG_LOCATION)
+        metadataProperties = new JSONPropertiesSource(metadataConfig).properties
+        def names = metadataProperties.findAll({it.key.contains('name')})
+        names.each {
+            metadataKeys.add(it.value)
+        }
+
+        Resource defaultConfig = resourceFactory.createResource(DEFAULT_CONFIG_LOCATION)
+        defaultProperties = new ResourcePropertiesSource(defaultConfig).properties
+    }
+
+    @Test
+    void verifyPropertiesInMetadataAreInDefault() {
+        def diff = metadataKeys.findAll {
+            defaultProperties.containsKey(it) ? null : it
+        }
+
+        assertEquals diff.size(), 7, "Missing keys in default config: ${diff}"
+        assertEquals diff.toString(), "[stormpath.application, stormpath.client.apiKey.fileIdPropertyName, " +
+                "stormpath.client.apiKey.fileSecretPropertyName, stormpath.enabled, stormpath.application.href, " +
+                "stormpath, stormpath.client]"
+    }
+
+    @Test
+    void verifyPropertiesInDefaultAreInMetadata() {
+        def diff = defaultProperties.findResults { k,v ->
+            metadataKeys.contains(k) ? null : k
+        }
+
+        assertEquals diff.size(), 0, "Missing keys in metadata config: ${diff}"
+    }
 }
