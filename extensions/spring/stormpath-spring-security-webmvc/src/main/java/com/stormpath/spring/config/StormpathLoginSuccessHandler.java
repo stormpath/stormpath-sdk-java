ceec9a2e2becb83e405c89834d28dadf8ec332c6
Julio Arias <jarias01@gmail.com>
2016-05-23 16:42:14 -0600

    Fixes #474
    Spring security support for access_token/refresh_token cookie

--- extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/config/StormpathLoginSuccessHandler.java
+++ extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/config/StormpathLoginSuccessHandler.java
@@ -16,15 +16,29 @@
 package com.stormpath.spring.config;
 
 import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.application.Application;
 import com.stormpath.sdk.authc.AuthenticationResult;
 import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.impl.oauth.authz.DefaultTokenResponse;
+import com.stormpath.sdk.oauth.AccessTokenResult;
+import com.stormpath.sdk.oauth.Authenticators;
+import com.stormpath.sdk.oauth.OAuthGrantRequestAuthenticationResult;
+import com.stormpath.sdk.oauth.OAuthRequestAuthentication;
+import com.stormpath.sdk.oauth.OAuthRequests;
+import com.stormpath.sdk.oauth.TokenResponse;
 import com.stormpath.sdk.servlet.authc.SuccessfulAuthenticationRequestEvent;
 import com.stormpath.sdk.servlet.authc.impl.DefaultSuccessfulAuthenticationRequestEvent;
 import com.stormpath.sdk.servlet.authc.impl.TransientAuthenticationResult;
 import com.stormpath.sdk.servlet.event.RequestEvent;
 import com.stormpath.sdk.servlet.event.impl.Publisher;
+import com.stormpath.sdk.servlet.filter.oauth.PasswordGrantAccessTokenResult;
 import com.stormpath.sdk.servlet.http.Saver;
 import com.stormpath.spring.security.provider.StormpathUserDetails;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.oltu.oauth2.common.message.types.TokenType;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -33,6 +47,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * @since 1.0.RC5
@@ -45,6 +60,12 @@ public class StormpathLoginSuccessHandler extends SavedRequestAwareAuthenticatio
 
     @Autowired
     private Publisher<RequestEvent> stormpathRequestEventPublisher;
+
+    @Autowired
+    private Application application;
+
+    @Autowired
+    private Client client;
 
     public StormpathLoginSuccessHandler(Client client, Saver<AuthenticationResult> saver) {
         this.stormpathClient = client;
@@ -63,8 +84,30 @@ public class StormpathLoginSuccessHandler extends SavedRequestAwareAuthenticatio
 
     protected void saveAccount(HttpServletRequest request, HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
         Account account = getAccount(authentication);
-        AuthenticationResult result = new TransientAuthenticationResult(account);
-        authenticationResultSaver.set(request, response, result);
+
+        String jwt = Jwts.builder()
+                             .setHeaderParam(JwsHeader.KEY_ID, client.getApiKey().getId())
+                             .setSubject(account.getHref())
+                             .setIssuedAt(new Date())
+                             .setIssuer(application.getHref())
+                             .setAudience(client.getApiKey().getId())
+                             .setExpiration(DateTime.now().plusMinutes(1).toDate())
+                             .claim("status", "AUTHENTICATED").signWith(SignatureAlgorithm.HS256, client.getApiKey().getSecret().getBytes("UTF-8")).compact();
+
+        OAuthRequestAuthentication authenticationRequest = OAuthRequests.IDSITE_AUTHENTICATION_REQUEST.builder().setToken(jwt).build();
+        OAuthGrantRequestAuthenticationResult authenticationResult = Authenticators.ID_SITE_AUTHENTICATOR.forApplication(application).authenticate(authenticationRequest);
+
+        authenticationResultSaver.set(request, response, createAccessTokenResult(authenticationResult));
+    }
+
+    protected AccessTokenResult createAccessTokenResult(final OAuthGrantRequestAuthenticationResult result) {
+        final TokenResponse tokenResponse =
+                DefaultTokenResponse.tokenType(TokenType.BEARER)
+                        .accessToken(result.getAccessTokenString())
+                        .refreshToken(result.getRefreshTokenString())
+                        .applicationHref(application.getHref())
+                        .expiresIn(String.valueOf(result.getExpiresIn())).build();
+        return new PasswordGrantAccessTokenResult(result.getAccessToken().getAccount(), tokenResponse);
     }
 
     protected Account getAccount(Authentication authentication) {
