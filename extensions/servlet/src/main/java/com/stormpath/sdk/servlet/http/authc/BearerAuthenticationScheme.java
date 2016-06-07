5fa535a256b742ad36bbc055bfe0f4ef5896fc97
Mario <mario@stormpath.com>
2016-06-07 16:58:56 -0300

    681 updated from SC_BAD_REQUEST to SC_UNAUTHORIZED

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/http/authc/BearerAuthenticationScheme.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/http/authc/BearerAuthenticationScheme.java
@@ -103,7 +103,9 @@ public class BearerAuthenticationScheme extends AbstractAuthenticationScheme {
             return result;
 
         } catch (OAuthException e) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); //Fix for https://github.com/stormpath/stormpath-sdk-java/issues/674
+            //updated from SC_BAD_REQUEST to SC_UNAUTHORIZED per https://github.com/stormpath/stormpath-sdk-java/issues/681
+            // and https://github.com/stormpath/stormpath-sdk-java/issues/674
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.setContentType("application/json;charset=UTF-8");
             response.setHeader("Cache-Control", "no-store");
             response.setHeader("Pragma", "no-cache");
