f84ecedbac9cdd9566a4e67c45675c76d708bf50
Mario <mario@stormpath.com>
2016-06-09 10:08:28 -0300

    679 removed test scope from yaml dependency

--- extensions/servlet/src/test/groovy/com/stormpath/sdk/servlet/config/impl/DefaultConfigFactoryTest.groovy
+++ extensions/servlet/src/test/groovy/com/stormpath/sdk/servlet/config/impl/DefaultConfigFactoryTest.groovy
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2016 Stormpath, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stormpath.sdk.servlet.config.impl
 
+import com.stormpath.sdk.lang.Classes
 import com.stormpath.sdk.servlet.config.Config
 import com.stormpath.sdk.servlet.config.ConfigLoader
 import org.springframework.mock.web.MockServletContext
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
+import org.yaml.snakeyaml.Yaml
 
 import java.lang.reflect.Field
 
 import static org.testng.Assert.assertEquals
+import static org.testng.Assert.assertNotNull
 
 /**
  * @since 1.0.RC9
@@ -53,6 +71,11 @@ class DefaultConfigFactoryTest {
         setEnv(env)
         config = new ConfigLoader().createConfig(new MockServletContext())
         assertEquals config.get('stormpath.baseUrl'), baseUrl
+    }
+
+    @Test
+    public void assertYAMLDependencyIsPresent() {
+        assertNotNull(Classes.forName(Yaml.class.getName()))
     }
 
     // From http://stackoverflow.com/a/496849
