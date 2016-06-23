77a2d1e76e5b3e7e4b965a6fd3a718ffe202035c
Miguel Zumbado <miguel.zumbado@edify.cr>
2016-06-23 12:32:55 -0600

    582 default message is empty string

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/ChangePasswordErrorModelFactory.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/ChangePasswordErrorModelFactory.java
@@ -14,6 +14,7 @@
  * limitations under the License.
  */package com.stormpath.sdk.servlet.mvc;
 
+import com.stormpath.sdk.lang.Strings;
 import com.stormpath.sdk.servlet.i18n.MessageSource;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +23,6 @@ import javax.servlet.http.HttpServletRequest;
  * @since 1.0.0
  */
 public class ChangePasswordErrorModelFactory extends AbstractErrorModelFactory {
-    private static final String INVALID_CHANGE_PASSWORD_MESSAGE = "stormpath.web.changePassword.form.errors.invalid";
 
     public ChangePasswordErrorModelFactory(MessageSource messageSource) {
         this.messageSource = messageSource;
@@ -30,7 +30,7 @@ public class ChangePasswordErrorModelFactory extends AbstractErrorModelFactory {
 
     @Override
     protected String getDefaultMessageKey() {
-        return INVALID_CHANGE_PASSWORD_MESSAGE;
+        return Strings.EMPTY_STRING;
     }
 
     @Override
