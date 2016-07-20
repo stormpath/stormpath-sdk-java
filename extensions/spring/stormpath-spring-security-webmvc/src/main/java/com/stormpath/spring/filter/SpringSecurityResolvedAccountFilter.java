0b310626ee855fd6622a33e324bab980aeb30bea
Matt Raible <matt@raibledesigns.com>
2016-07-20 12:01:51 -0600

    Added unit test and updated integration test to expect an account.getHref() comparison to be made.

--- extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/filter/SpringSecurityResolvedAccountFilter.java
+++ extensions/spring/stormpath-spring-security-webmvc/src/main/java/com/stormpath/spring/filter/SpringSecurityResolvedAccountFilter.java
@@ -26,33 +26,31 @@ public class SpringSecurityResolvedAccountFilter extends HttpFilter implements I
     @Autowired
     private AuthenticationProvider authenticationProvider;
 
+    private AccountResolver accountResolver = AccountResolver.INSTANCE;
+
     @Override
     protected void filter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
         throws Exception {
 
-        //Fix for https://github.com/stormpath/stormpath-sdk-java/issues/605
-        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+        Account account = accountResolver.getAccount(request);
 
-            Account account = AccountResolver.INSTANCE.getAccount(request);
+        if (account != null) {
+            Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
 
-            if (account != null) {
-                Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
+            boolean forceRefresh;
 
-                boolean forceRefresh;
+            if (currentAuthentication == null || !(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof StormpathUserDetails)) {
+                forceRefresh = true;
+            } else {
+                StormpathUserDetails userDetails = (StormpathUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                forceRefresh = !userDetails.getProperties().get("href").equals(account.getHref());
+            }
 
-                if (currentAuthentication == null || !(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof StormpathUserDetails)) {
-                    forceRefresh = true;
-                } else {
-                    StormpathUserDetails userDetails = (StormpathUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-                    forceRefresh = !userDetails.getProperties().get("href").equals(account.getHref());
-                }
-
-                if (forceRefresh) {
-                    Authentication authentication = new ProviderAuthenticationToken(account);
-                    authentication = authenticationProvider.authenticate(authentication);
-                    SecurityContextHolder.clearContext();
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
+            if (forceRefresh) {
+                Authentication authentication = new ProviderAuthenticationToken(account);
+                authentication = authenticationProvider.authenticate(authentication);
+                SecurityContextHolder.clearContext();
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
 
