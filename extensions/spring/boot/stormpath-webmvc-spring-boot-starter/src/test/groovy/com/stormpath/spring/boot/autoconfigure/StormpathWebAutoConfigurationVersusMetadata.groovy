046dddd33c2b4a5f67e933aa15612bfbf45181c5
Matt Raible <matt@raibledesigns.com>
2016-08-16 12:50:03 -0600

    Match @Value annotations with IDE hints in additional-spring-configuration-metadata.json. Wrote tests to confirm matches and to generate Spring Boot documentation from metadata.json.

--- extensions/spring/boot/stormpath-webmvc-spring-boot-starter/src/test/groovy/com/stormpath/spring/boot/autoconfigure/StormpathWebAutoConfigurationVersusMetadata.groovy
+++ extensions/spring/boot/stormpath-webmvc-spring-boot-starter/src/test/groovy/com/stormpath/spring/boot/autoconfigure/StormpathWebAutoConfigurationVersusMetadata.groovy
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.stormpath.sdk.impl.io.ClasspathResource
 import com.stormpath.sdk.impl.io.Resource
+import com.stormpath.spring.config.AccessTokenCookieProperties
+import com.stormpath.spring.config.RefreshTokenCookieProperties
+import com.stormpath.spring.mvc.*
 import org.springframework.beans.factory.annotation.Value
 import org.testng.annotations.Test
 
@@ -11,13 +14,12 @@ import java.lang.annotation.Annotation
 import java.lang.reflect.Field
 
 import static org.testng.Assert.assertEquals
-
 /**
- * This test is designed to compare {@link StormpathAutoConfiguration} with additional-spring-configuration-metadata.json
+ * This test is designed to compare {@link StormpathWebMvcAutoConfiguration} with additional-spring-configuration-metadata.json
  * to make sure all properties are documented.
  */
 
-class StormpathAutoConfigurationVersusMetadata {
+class StormpathWebAutoConfigurationVersusMetadata {
 
     @Test
     def void verifyPropertiesInValuesAreInMetadata() {
@@ -26,6 +28,10 @@ class StormpathAutoConfigurationVersusMetadata {
 
         def diff = valueNames.findAll {
             metaNames.contains(it) ? null : it
+        }
+
+        diff.each {
+            println it
         }
 
         assertEquals diff.size(), 0, "Missing @Value annotations in metadata: ${diff}"
@@ -40,8 +46,11 @@ class StormpathAutoConfigurationVersusMetadata {
             valueNames.contains(it) ? null : it
         }
 
-        assertEquals diff.size(), 3, "Missing metadata properties in @Values: ${diff}"
-        assertEquals diff.sort().toString(), "[stormpath.client.cacheManager.defaultTti, stormpath.client.cacheManager.defaultTtl, stormpath.enabled]"
+        // stormpath.application.href is in AbstractStormpathConfiguration.java
+        // idSite.resultUri in StormpathWebSecurityConfigurer.java
+        // *.fieldOrder used in AbstractControllerConfig, but not in @Value annotation
+        assertEquals diff.size(), 4, "Missing metadata properties in @Values:"
+        assertEquals diff.sort().toString(), "[stormpath.application.href, stormpath.web.idSite.resultUri, stormpath.web.login.form.fieldOrder, stormpath.web.register.form.fieldOrder]"
     }
 
     @Test
@@ -60,15 +69,24 @@ class StormpathAutoConfigurationVersusMetadata {
             sb.append(it.description + "\n\n")
         }
 
-        File file = new File("../docs/source/appendix/spring-boot-core-properties.rst");
+        File file = new File("../docs/source/appendix/spring-boot-web-properties.rst");
         file.write sb.toString()
+    }
+
+
+
+    @Test
+    public void compareValueAnnotationsWithWebStormpathProperties() {
+
     }
 
     static def Set getPropertyNamesFromMetadata() throws IOException {
         def props = getPropertiesFromMetadata()
         def names = new HashSet()
         props.each {
-            names << it.name
+            if (!it.name.contains("form.fields")) {
+                names << it.name
+            }
         }
 
         names
@@ -87,14 +105,31 @@ class StormpathAutoConfigurationVersusMetadata {
     }
 
     static def Set getPropertiesFromConfig() {
-        Set<Field> valueFields = findFields(StormpathAutoConfiguration.class, Value)
+        Set<Field> valueFields = findFields(StormpathWebMvcAutoConfiguration.class, Value)
+        // add additional fields from other configuration classes
+        //valueFields.addAll(findFields(AbstractStormpathConfiguration.class, Value))
+        valueFields.addAll(findFields(AccessTokenCookieProperties.class, Value))
+        valueFields.addAll(findFields(ChangePasswordControllerConfig.class, Value))
+        valueFields.addAll(findFields(ForgotPasswordControllerConfig.class, Value))
+        valueFields.addAll(findFields(LoginControllerConfig.class, Value))
+        valueFields.addAll(findFields(LogoutControllerConfig.class, Value))
+        valueFields.addAll(findFields(RefreshTokenCookieProperties.class, Value))
+        valueFields.addAll(findFields(RegisterControllerConfig.class, Value))
+        valueFields.addAll(findFields(VerifyControllerConfig.class, Value))
         def names = new HashSet()
         for (Field field : valueFields) {
             Value value = field.getAnnotation(Value.class);
             def match = value.value() =~ /'([^']*)'/
             match.each {
-                //println it[1]
-                names << it[1]
+                // 1. if key doesn't start with stormpath, ignore it
+                // 2. if key contains 'form.fields', ignore it
+                // it should be possible to do this with the above regex, but I was unable to make it work
+                if (it[1].startsWith("stormpath") && !it[1].contains("form.fields")) {
+                    // the regex above captures some values instead of keys, so ignore thoses
+                    if (!it[1].contains("stormpath/")) {
+                        names << it[1]
+                    }
+                }
             }
         }
 
