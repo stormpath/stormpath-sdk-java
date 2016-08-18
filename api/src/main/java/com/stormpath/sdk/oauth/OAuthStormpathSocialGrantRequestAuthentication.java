0ae607be8f52a8d9e160729377aacb55be98061e
Matt Raible <matt@raibledesigns.com>
2016-08-18 15:09:54 -0600

    Fix @since to be 1.1.0

--- api/src/main/java/com/stormpath/sdk/oauth/OAuthStormpathSocialGrantRequestAuthentication.java
+++ api/src/main/java/com/stormpath/sdk/oauth/OAuthStormpathSocialGrantRequestAuthentication.java
@@ -4,7 +4,7 @@ package com.stormpath.sdk.oauth;
  * This class represents a request for Stormpath to authenticate an Account and exchange its api key and api secret for a valid OAuth 2.0 access token.
  * Using stormpath_social grant type
  *
- * @since 1.0.0
+ * @since 1.1.0
  */
 public interface OAuthStormpathSocialGrantRequestAuthentication extends OAuthGrantRequestAuthentication {
     String getApiKeyId();
