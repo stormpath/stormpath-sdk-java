7d7010dba4b9753565234fcbedca10f9ce7b47d6
Micah Silverman <micah@afitnerd.com>
2016-08-18 17:34:47 -0400

    Added copyright info to all files and made various formatting changes per @mriable

--- examples/spring-boot-webmvc-angular/src/main/java/com/stormpath/spring/boot/examples/AngularController.java
+++ examples/spring-boot-webmvc-angular/src/main/java/com/stormpath/spring/boot/examples/AngularController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Stormpath, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stormpath.spring.boot.examples;
 
 import org.springframework.stereotype.Controller;
@@ -10,7 +25,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 public class AngularController {
 
-
     @RequestMapping(
         path = { "/profile", "/login", "/register", "/forgot", "/verify", "/change" },
         method = GET,
@@ -22,5 +36,4 @@ public class AngularController {
 
         return "forward:/";
     }
-
 }
