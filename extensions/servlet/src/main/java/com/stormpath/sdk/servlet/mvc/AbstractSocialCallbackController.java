c9c474e2dc3c8040c51819584b1ee839a0f3f8d2
Julio Arias <jarias@users.noreply.github.com>
2016-08-19 09:13:24 -0600

    SAML and Social Login does not redirect the user back to the intercepted pre-login uri
    Fixes #849

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/AbstractSocialCallbackController.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/AbstractSocialCallbackController.java
@@ -19,12 +19,14 @@ import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.account.AccountStatus;
 import com.stormpath.sdk.authc.AuthenticationResult;
 import com.stormpath.sdk.lang.Assert;
+import com.stormpath.sdk.lang.Strings;
 import com.stormpath.sdk.provider.ProviderAccountRequest;
 import com.stormpath.sdk.provider.ProviderAccountResult;
 import com.stormpath.sdk.servlet.authc.impl.DefaultSuccessfulAuthenticationRequestEvent;
 import com.stormpath.sdk.servlet.authc.impl.TransientAuthenticationResult;
 import com.stormpath.sdk.servlet.config.Config;
 import com.stormpath.sdk.servlet.http.Saver;
+import com.stormpath.sdk.servlet.util.ServletUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -73,6 +75,13 @@ public abstract class AbstractSocialCallbackController extends AbstractControlle
 
         eventPublisher.publish(new DefaultSuccessfulAuthenticationRequestEvent(request, response, null, authcResult));
 
-        return new DefaultViewModel(nextUri).setRedirect(true);
+        //Fixes #849 we send in the state the original path the user requested based on the next query param, so we can redirect back
+        String redirectUri = nextUri;
+        String next = ServletUtils.getCleanParam(request, "state");
+        if (Strings.hasText(next)) {
+            redirectUri = next;
+        }
+
+        return new DefaultViewModel(redirectUri).setRedirect(true);
     }
 }
