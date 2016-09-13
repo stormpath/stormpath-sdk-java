162f2e7a582e42011a314f5b6fac5639e438d71b
Les Hazlewood <les@hazlewood.com>
2016-09-13 11:25:14 -0700

    added test support for the zuul module

---
+++ extensions/zuul/src/test/groovy/com/stormpath/zuul/filter/AppliedRequestHeaderFilterTest.groovy
@@ -0,0 +1,12 @@
+package com.stormpath.zuul.filter
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+class AppliedRequestHeaderFilterTest {
+
+    @Test
+    void test() {
+        println "Testing!"
+    }
+}
