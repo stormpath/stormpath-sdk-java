d5931dc9e2ec79792c4d78a5b823573957abb89e
Ramesh Bhojan <ramesh@stormpath.com>
2016-09-14 12:39:01 -0700

    #966 -minor change in javadoc

--- api/src/main/java/com/stormpath/sdk/account/AccountLinker.java
+++ api/src/main/java/com/stormpath/sdk/account/AccountLinker.java
@@ -1,9 +1,8 @@
 package com.stormpath.sdk.account;
 
 /**
- * Any AccountStoreMappable that can have an AccountLinkingPolicy
- * should implement this interface to be able to get/update its
- * AccountLinkingPolicy. e.g. Applications, Organizations can have
+ * Any AccountStoreMappable that can have an AccountLinkingPolicy implements this interface
+ * to be able to get/update its AccountLinkingPolicy. e.g. Applications, Organizations can have
  * a AccountLinkingPolicy and therefore should implement this interface.
  *
  * @since 1.1.0
