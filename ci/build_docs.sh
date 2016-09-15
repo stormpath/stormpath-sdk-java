0791f1edd19ce9d3284e95efcc84d497cd660c6a
Micah Silverman <micah@afitnerd.com>
2016-09-15 06:43:08 -0700

    updated EXIT_CODE in build_docs

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -8,7 +8,7 @@ source ./ci/common.sh
 info "Generating servlet plugin docs..."
 $(cd docs && make html &> $WORKDIR/target/servlet-plugin-docs.log)
 if [ $? -ne 0 ]; then
-  EXIT_CODE = $?
+  EXIT_CODE=$?
   error "Error generating servlet plugin docs"
   cat $WORKDIR/target/servlet-plugin-docs.log
   exit EXIT_CODE
@@ -18,7 +18,7 @@ fi
 info "Generating SpringBoot extension docs..."
 $(cd extensions/spring/boot/docs && make html &> $WORKDIR/spring-boot-docs.log)
 if [ $? -ne 0 ]; then
-  EXIT_CODE = $?
+  EXIT_CODE=$?
   error "Error generating SpringBoot plugin docs"
   cat $WORKDIR/target/spring-boot-docs.log
   exit EXIT_CODE
@@ -28,7 +28,7 @@ info "Generating JavaDocs..."
 # javadocs
 mvn -q javadoc:aggregate -P travis-docs &> $WORKDIR/target/javadocs.log
 if [ $? -ne 0 ]; then
-  EXIT_CODE = $?
+  EXIT_CODE=$?
   error "Error generating JavaDocs"
   cat $WORKDIR/target/javadocs.log
   exit EXIT_CODE
