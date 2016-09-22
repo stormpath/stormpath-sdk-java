1f44a491046f8add2b4d0a9ca008456e165e3b83
Mehrshad Rafiei <mehrshad@stormpath.com>
2016-09-22 11:51:58 -0700

    Committing some fixes to review comments.

--- api/src/main/java/com/stormpath/sdk/factor/sms/SmsFactor.java
+++ api/src/main/java/com/stormpath/sdk/factor/sms/SmsFactor.java
@@ -23,6 +23,8 @@ import com.stormpath.sdk.factor.Factor;
 import com.stormpath.sdk.phone.Phone;
 import com.stormpath.sdk.resource.ResourceException;
 
+import java.util.Map;
+
 /**
  * An {@code SmsFactor} is a Factor that represents a phone used in SMS-based challenge workflows.
  * When issuing a challenge via an SmsFactor, a code is sent via SMS to the phone, and the user
@@ -99,6 +101,34 @@ public interface SmsFactor extends Factor {
      * @since 1.1.0
      */
     ChallengeList getChallenges(ChallengeCriteria criteria);
+
+    /**
+     * Returns a paginated list of the sms factor's assigned challenges that match the specified query criteria.
+     *
+     * <p>This method is mostly provided as a non-type-safe alternative to the
+     * {@link #getChallenges(ChallengeCriteria criteria)} method which might be useful in dynamic languages on
+     * the
+     * JVM (for example, with Groovy):
+     * <pre>
+     * def challenges = smsFactor.getChallenges([createdAt: '2016-01-01', orderBy: 'createdAt desc', limit: 5])
+     * </pre>
+     * The query parameter names and values must be equal to those documented in the Stormpath REST API product guide.
+     * <p/>
+     * Each {@code queryParams} key/value pair will be converted to String name to String value pairs and appended to
+     * the resource URL as query parameters, for example:
+     * <pre>
+     * .../factors/factorId/challenges?param1=value1&...
+     * </pre>
+     * </p>
+     * If in doubt, use {@link #getChallenges(ChallengeCriteria criteria)}  as all possible query options are
+     * available
+     * via type-safe guarantees that can be auto-completed by most IDEs.
+     *
+     * @param queryParams the query parameters to use when performing a request to the collection.
+     * @return a paginated list of the smsFactor's challenges that match the specified query criteria.
+     * @since 1.1.0
+     */
+    ChallengeList getChallenges(Map<String, Object> queryParams);
 
     /**
      * Challenges this {@code SmsFactor}.
