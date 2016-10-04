d6c0689a94cfb682bdd15eb4e6a51e97ee5936dd
Micah Silverman <micah@afitnerd.com>
2016-10-04 09:29:40 -0400

    Updated publish to s3

--- ci/transfer_logs_to_s3.sh
+++ ci/transfer_logs_to_s3.sh
@@ -11,6 +11,7 @@ TSTAMP=`date "+%Y-%m-%d_%H-%M-%S"`
 S3_BUCKET="$S3_BASE_BUCKET/$TSTAMP-$TRAVIS_JOB_ID"
 
 echo "writing logs to: $S3_BUCKET/tests.log"
+echo "you can access the log at: http://jsdk-travis-ci-build-logs.s3-website-us-east-1.amazonaws.com/$TSTAMP-$TRAVIS_JOB_ID/tests.log"
 
 aws s3 mb $S3_BUCKET
 aws s3 cp "$WORKDIR/target/tests.log" $S3_BUCKET/tests.log
