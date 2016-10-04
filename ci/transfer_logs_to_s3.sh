52dbaf4d702ac61696792805928a3d578ed74404
Micah Silverman <micah@afitnerd.com>
2016-10-04 08:21:27 -0400

    Updated publish to s3

--- ci/transfer_logs_to_s3.sh
+++ ci/transfer_logs_to_s3.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:~/usr/local/bin
 
 S3_BASE_BUCKET=s3://jsdk-travis-ci-build-logs
 TSTAMP=`date "+%Y-%m-%d_%H-%M-%S"`
-S3_BUCKET="$S3_BUCKET/$TSTAMP-$TRAVIS_JOB_ID"
+S3_BUCKET="$S3_BASE_BUCKET/$TSTAMP-$TRAVIS_JOB_ID"
 
 echo "writing logs to: $S3_BUCKET/tests.log"
 
