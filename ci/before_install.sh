c12767499271867f1ca64329fc3ae4de0d0665e4
Matt Raible <matt@raibledesigns.com>
2016-11-02 13:28:37 -0600

    Install Maven in before_install.sh

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -13,10 +13,16 @@ export RELEASE_VERSION="$(xmllint --xpath "//*[local-name()='project']/*[local-n
 export IS_RELEASE="$([ ${RELEASE_VERSION/SNAPSHOT} == $RELEASE_VERSION ] && [ $TRAVIS_BRANCH == 'master' ] && echo 'true')"
 export BUILD_DOCS="$([ $TRAVIS_JDK_VERSION == 'oraclejdk8' ] && echo 'true')"
 export RUN_ITS="$([ $TRAVIS_JDK_VERSION == 'openjdk7' ] && echo 'true')"
+#Install Maven 3.3.9 since Travis uses 3.2 by default
+wget https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
+unzip -qq apache-maven-3.3.9-bin.zip
+export M2_HOME=$PWD/apache-maven-3.3.9
+export PATH=$M2_HOME/bin:$PATH
 
 info "Build configuration:"
 echo "Version:             $RELEASE_VERSION"
 echo "Is release:          ${IS_RELEASE:-false}"
 echo "Build documentation: ${BUILD_DOCS:-false}"
+echo "Running IT tests:    ${RUN_ITS:-false}"
 echo "Running IT tests:    ${RUN_ITS:-false}"
 
