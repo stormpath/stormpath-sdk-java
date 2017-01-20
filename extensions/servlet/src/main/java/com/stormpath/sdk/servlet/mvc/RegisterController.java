dbc7aa5f3a60fd86c53ddc517a3d4ad69f4dfc03
Peter <peter@stormpath.com>
2017-01-19 16:30:50 -0800

    issue-1251 adding passwordPolicy to registration page response

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/RegisterController.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/RegisterController.java
@@ -22,10 +22,16 @@ import com.stormpath.sdk.authc.AuthenticationResult;
 import com.stormpath.sdk.cache.Cache;
 import com.stormpath.sdk.client.Client;
 import com.stormpath.sdk.directory.AccountStore;
+import com.stormpath.sdk.directory.AccountStoreVisitor;
 import com.stormpath.sdk.directory.AccountStoreVisitorAdapter;
 import com.stormpath.sdk.directory.Directory;
+import com.stormpath.sdk.directory.PasswordStrength;
+import com.stormpath.sdk.group.Group;
+import com.stormpath.sdk.impl.resource.AbstractResource;
 import com.stormpath.sdk.lang.Assert;
+import com.stormpath.sdk.lang.Strings;
 import com.stormpath.sdk.organization.Organization;
+import com.stormpath.sdk.organization.OrganizationList;
 import com.stormpath.sdk.servlet.account.event.impl.DefaultRegisteredAccountRequestEvent;
 import com.stormpath.sdk.servlet.application.ApplicationResolver;
 import com.stormpath.sdk.servlet.authc.impl.TransientAuthenticationResult;
@@ -42,6 +48,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -154,6 +161,7 @@ public class RegisterController extends FormController {
             model.put("loginUri", loginUri);
         } else {
             model.put("accountStores", accountStoreModelFactory.getAccountStores(request));
+            model.put("passwordPolicy", getPasswordPolicy(request));
         }
     }
 
@@ -309,5 +317,99 @@ public class RegisterController extends FormController {
         } //If not a map ignore, the spec doesn't cover this case
 
         return result;
+    }
+
+    private Map<String, Object> getPasswordPolicy(HttpServletRequest request) {
+
+        String onk = request.getParameter("organizationNameKey");
+
+        PasswordStrength passwordStrength;
+
+        if (Strings.hasText(onk)) {
+             passwordStrength = findPasswordStrengthByOrganization(onk);
+        } else {
+            passwordStrength = getApplicationPasswordStrength(request);
+        }
+
+        if (passwordStrength == null) {
+            return null;
+        }
+
+        return convertPasswordStrengthToMap(passwordStrength);
+    }
+
+    private PasswordStrength getApplicationPasswordStrength(HttpServletRequest request) {
+        AccountStore defaultAccountStore = getApplication(request).getDefaultAccountStore();
+
+        if (defaultAccountStore == null) {
+            return null;
+        }
+
+        final PasswordStrength[] passwordStrength = new PasswordStrength[1];
+        defaultAccountStore.accept(new AccountStoreVisitor() {
+            @Override
+            public void visit(Group group) {
+                passwordStrength[0] = group.getDirectory().getPasswordPolicy().getStrength();
+            }
+
+            @Override
+            public void visit(Directory directory) {
+                passwordStrength[0] = directory.getPasswordPolicy().getStrength();
+            }
+
+            @Override
+            public void visit(Organization organization) {
+                passwordStrength[0] = getOrganizationPasswordStrength(organization);
+            }
+        });
+
+        return passwordStrength[0];
+    }
+
+    private PasswordStrength findPasswordStrengthByOrganization(String onk) {
+        HashMap<String, Object> query = new HashMap<>();
+        query.put("nameKey", onk);
+        OrganizationList organizations = client.getOrganizations(query);
+
+        if (organizations.getSize() != 1) {
+            return null;
+        }
+
+        return getOrganizationPasswordStrength(organizations.single());
+    }
+
+    private PasswordStrength getOrganizationPasswordStrength(Organization organization) {
+        AccountStore organizationDefaultAccountStore = organization.getDefaultAccountStore();
+
+        if (organizationDefaultAccountStore == null) {
+            return null;
+        }
+
+        final PasswordStrength[] passwordStrength = new PasswordStrength[1];
+        organizationDefaultAccountStore.accept(new AccountStoreVisitorAdapter() {
+            @Override
+            public void visit(Group group) {
+                passwordStrength[0] = group.getDirectory().getPasswordPolicy().getStrength();
+            }
+
+            @Override
+            public void visit(Directory directory) {
+                passwordStrength[0] = directory.getPasswordPolicy().getStrength();
+            }
+        });
+
+        return passwordStrength[0];
+    }
+
+    private Map<String, Object> convertPasswordStrengthToMap(PasswordStrength passwordStrength) {
+        AbstractResource abstractResource = (AbstractResource) passwordStrength;
+
+        Map<String, Object> strength = new HashMap<>();
+
+        for (String propertyName : abstractResource.getPropertyDescriptors().keySet()) {
+            strength.put(propertyName, abstractResource.getProperty(propertyName));
+        }
+
+        return strength;
     }
 }
