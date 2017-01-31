a95bb9d7ff0b1e7cbdd05b6923ac9609cddd481b
Peter <peter@stormpath.com>
2017-01-31 11:53:29 -0800

    issue-1257 fixing unit test

--- impl/src/test/groovy/com/stormpath/sdk/impl/oauth/DefaultCreateOAuthPasswordGrantAuthenticationAttemptTest.groovy
+++ impl/src/test/groovy/com/stormpath/sdk/impl/oauth/DefaultCreateOAuthPasswordGrantAuthenticationAttemptTest.groovy
@@ -39,11 +39,12 @@ class DefaultCreateOAuthPasswordGrantAuthenticationAttemptTest {
 
         def propertyDescriptors = defaultCreateGrantAuthAttempt.getPropertyDescriptors()
 
-        assertEquals(propertyDescriptors.size(), 4)
+        assertEquals(propertyDescriptors.size(), 5)
 
         assertTrue(propertyDescriptors.get("username") instanceof StringProperty)
         assertTrue(propertyDescriptors.get("password") instanceof StringProperty)
         assertTrue(propertyDescriptors.get("accountStore") instanceof StringProperty)
+        assertTrue(propertyDescriptors.get("organizationNameKey") instanceof StringProperty)
         assertTrue(propertyDescriptors.get("grant_type") instanceof StringProperty)
     }
 
