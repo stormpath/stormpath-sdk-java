85797f05805eee1e4457c5e47a7caca27a958ecd
Mehrshad Rafiei <mehrshad@stormpath.com>
2017-02-06 20:08:52 -0800

    penid-connect-new-OAuthPolicy-fields Review Comments

--- api/src/main/java/com/stormpath/sdk/oauth/OAuthPolicy.java
+++ api/src/main/java/com/stormpath/sdk/oauth/OAuthPolicy.java
@@ -51,6 +51,7 @@ public interface OAuthPolicy extends Resource, Saveable {
      * Returns the Time To Live for the id tokens created for the parent {@link Application Application} expressed in a period of time format, for example: PT1H.
      *
      * @return the String representation of the Time To Live for the id tokens created for the parent {@link Application Application}
+     * @since 1.6.0
      */
     String getIdTokenTtl();
 
@@ -87,6 +88,7 @@ public interface OAuthPolicy extends Resource, Saveable {
      * Sets the Time To Live for the id tokens created for the parent {@link Application Application} expressed in a period of time format, for example: PT1H.
      *
      * @return this instance for method chaining.
+     * @since 1.6.0
      */
     OAuthPolicy setIdTokenTtl(String idTokenTtl);
 
@@ -94,7 +96,7 @@ public interface OAuthPolicy extends Resource, Saveable {
      * Creates a new {@link Scope} assigned to this oauthPolicy in the Stormpath server and returns the created resource.
      * The scope is used for openid connect flows.
      *
-     * @param scope {@link Scope} pojo to hold necessary data to send to the back- end to create a {@link Scope}.
+     * @param scope {@link Scope} pojo to hold necessary data to send to the back-end to create a {@link Scope}.
      * @return the newly created {@link Scope}.
      *
      * @since 1.6.0
@@ -112,6 +114,8 @@ public interface OAuthPolicy extends Resource, Saveable {
 
     /**
      * Returns access token attribute mappings.
+     * Open Id provider (OP) would enter any custom mappings used for their internal purposes in this map.
+     * Authorization server would then add all these mappings as part of the access token upon its generation.
      *
      * @return access token attribute mappings.
      *
@@ -123,6 +127,8 @@ public interface OAuthPolicy extends Resource, Saveable {
      * Sets access token attribute mappings to be inserted into access tokens.
      *
      * @param accessTokenAttributeMap access token attribute mappings to be inserted into access tokens
+     * Open Id provider (OP) would enter any custom mappings used for their internal purposes in this map.
+     * Authorization server would then add all these mappings as part of the access token upon its generation.
      * @return this instance for method chaining.
      *
      * @since 1.6.0
@@ -131,6 +137,8 @@ public interface OAuthPolicy extends Resource, Saveable {
 
     /**
      * Returns id token attribute mappings.
+     * Open Id provider (OP) would enter any custom mappings used for their internal purposes in this map.
+     * Authorization server would then add all these mappings as part of the id token upon its generation.
      *
      * @return id token attribute mappings.
      *
@@ -142,6 +150,8 @@ public interface OAuthPolicy extends Resource, Saveable {
      * Sets id token attribute mappings to be inserted into id tokens.
      *
      * @param idTokenAttributeMap id token attribute mappings to be inserted into id tokens
+     * Open Id provider (OP) would enter any custom mappings used for their internal purposes in this map.
+     * Authorization server would then add all these mappings as part of the id token upon its generation.
      * @return this instance for method chaining.
      *
      * @since 1.6.0
